e0a95ea004513a8274c461516feb22eb592445ce
fkorotkov <fkorotkov@twitter.com>
2014-03-02 13:19:39 -0800

    treat pex files as archive

---
+++ src/com/twitter/intellij/pants/file/PexFileTypeFactory.java
@@ -0,0 +1,13 @@
+package com.twitter.intellij.pants.file;
+
+import com.intellij.ide.highlighter.ArchiveFileType;
+import com.intellij.openapi.fileTypes.FileTypeConsumer;
+import com.intellij.openapi.fileTypes.FileTypeFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class PexFileTypeFactory extends FileTypeFactory {
+    @Override
+    public void createFileTypes(@NotNull FileTypeConsumer consumer) {
+        consumer.consume(ArchiveFileType.INSTANCE, "pex");
+    }
+}
