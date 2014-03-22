da33d8259b22511db666eadd50afb7ad2dbaba3f
fkorotkov <fkorotkov@twitter.com>
2014-03-22 15:30:46 -0700

    removed pants executable setting. will be populated from BUILD file.

--- src/com/twitter/intellij/pants/settings/PantsProjectSettings.java
+++ src/com/twitter/intellij/pants/settings/PantsProjectSettings.java
@@ -8,28 +8,9 @@ import org.jetbrains.annotations.Nullable;
  * Created by fedorkorotkov
  */
 public class PantsProjectSettings extends ExternalProjectSettings {
-  @Nullable
-  private String pantsExecutablePath = null;
-
-  public PantsProjectSettings() {
-  }
-
-  public PantsProjectSettings(@Nullable String pantsExecutablePath) {
-    this.pantsExecutablePath = pantsExecutablePath;
-  }
-
-  @Nullable
-  public String getPantsExecutablePath() {
-    return pantsExecutablePath;
-  }
-
-  public void setPantsExecutablePath(@Nullable String pantsExecutablePath) {
-    this.pantsExecutablePath = pantsExecutablePath;
-  }
-
   @NotNull
   @Override
   public PantsProjectSettings clone() {
-    return new PantsProjectSettings(pantsExecutablePath);
+    return new PantsProjectSettings();
   }
 }
