3e813506ff5ef26d452dd8fdf1f4f503ac743e38
Fedor Korotkov <fedor.korotkov@gmail.com>
2014-09-03 20:22:50 -0700

    fixed tree structure provider
    it wasn't working. now there is a BUILD file node under a module node in a project view. to distinguish it with the real files I changed an icon to the pants logo.
    
    Testing Done:
    locally
    
    Reviewed at https://rbcommons.com/s/twitter/r/966/

--- src/com/twitter/intellij/pants/projectview/PantsTreeStructureProvider.java
+++ src/com/twitter/intellij/pants/projectview/PantsTreeStructureProvider.java
@@ -1,5 +1,6 @@
 package com.twitter.intellij.pants.projectview;
 
+import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.projectView.TreeStructureProvider;
 import com.intellij.ide.projectView.ViewSettings;
 import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode;
@@ -7,14 +8,15 @@ import com.intellij.ide.projectView.impl.nodes.PsiFileNode;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
 import com.intellij.openapi.externalSystem.util.ExternalSystemConstants;
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Condition;
-import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.util.containers.ContainerUtil;
+import com.twitter.intellij.pants.util.PantsUtil;
+import icons.PantsIcons;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -29,25 +31,19 @@ public class PantsTreeStructureProvider implements TreeStructureProvider {
   @NotNull
   @Override
   public Collection<AbstractTreeNode> modify(
-    final AbstractTreeNode node,
-    Collection<AbstractTreeNode> collection,
+    @NotNull final AbstractTreeNode node,
+    @NotNull Collection<AbstractTreeNode> collection,
     ViewSettings settings
   ) {
     final Project project = node.getProject();
     if (node instanceof PsiDirectoryNode && project != null) {
-      final Module module = ContainerUtil.find(
-        ModuleManager.getInstance(project).getModules(), new Condition<Module>() {
-          @Override
-          public boolean value(Module module) {
-            final VirtualFile moduleFile = module.getModuleFile();
-            return moduleFile != null && moduleFile.getParent().equals(((PsiDirectoryNode) node).getVirtualFile());
-          }
-        }
-      );
-      String buildPath = module != null ? module.getOptionValue(ExternalSystemConstants.LINKED_PROJECT_PATH_KEY) : null;
+      final Module module = ModuleUtil.findModuleForPsiElement(((PsiDirectoryNode)node).getValue());
+      final VirtualFile moduleFile = module != null ? module.getModuleFile() : null;
+      String buildPath = moduleFile != null && moduleFile.getParent().equals(((PsiDirectoryNode)node).getVirtualFile())?
+                         module.getOptionValue(ExternalSystemConstants.LINKED_PROJECT_PATH_KEY) : null;
       if (buildPath != null) {
-        buildPath = node.getProject().getBaseDir().getParent().getPath() + "/" + buildPath;
-        final VirtualFile buildFile = LocalFileSystem.getInstance().findFileByPath(buildPath);
+        final VirtualFile pantsExecutable = PantsUtil.findPantsExecutable(moduleFile);
+        final VirtualFile buildFile = pantsExecutable != null ? pantsExecutable.getParent().findFileByRelativePath(buildPath) : null;
         if (buildFile != null) {
           // Check if there's already a BUILD file in the directory; if so, we don't add another
           final AbstractTreeNode existingBuildFile = ContainerUtil.find(
@@ -59,8 +55,14 @@ public class PantsTreeStructureProvider implements TreeStructureProvider {
             }
           );
           if (existingBuildFile == null) {
-            final PsiFile buildPsiFile = PsiManager.getInstance(node.getProject()).findFile(buildFile);
-            final PsiFileNode buildNode = new PsiFileNode(node.getProject(), buildPsiFile, settings);
+            final PsiFile buildPsiFile = PsiManager.getInstance(project).findFile(buildFile);
+            final PsiFileNode buildNode = new PsiFileNode(project, buildPsiFile, settings) {
+              @Override
+              protected void updateImpl(PresentationData data) {
+                super.updateImpl(data);
+                data.setIcon(PantsIcons.Icon);
+              }
+            };
             final List<AbstractTreeNode> modifiedCollection = new ArrayList<AbstractTreeNode>(collection);
             modifiedCollection.add(buildNode);
             return modifiedCollection;
