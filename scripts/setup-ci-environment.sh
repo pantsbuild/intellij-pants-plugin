400fbcee144798f45449738d2b95596d1e2d3c65
Eric Ayers <zundel@squareup.com>
2015-02-26 07:20:52 -0500

    Update README.md with latest instructions, changed setup-ci-environment.sh to use curl so it would work on my Mac

--- scripts/setup-ci-environment.sh
+++ scripts/setup-ci-environment.sh
@@ -4,6 +4,11 @@ set -e
 
 CWD=$(pwd)
 
+if [[ -z "$IJ_BUILD_NUMBER" || -z "$IJ_VERSION" ]] ; then
+  echo "Must set IJ_BUILD_NUMBER and IJ_VERSION in the environment."
+  exit 1
+fi
+
 # we will use Community ids to download plugins.
 SCALA_PLUGIN_ID="org.intellij.scala"
 PYTHON_PLUGIN_ID="PythonCore"
@@ -18,7 +23,7 @@ mkdir -p .cache/intellij
 
 if [ ! -d .cache/intellij/idea-dist ]; then
   echo "Loading $IJ_BUILD..."
-  wget http://download-cf.jetbrains.com/idea/idea${IJ_BUILD}.tar.gz
+  curl -L -o idea${IJ_BUILD}.tar.gz http://download-cf.jetbrains.com/idea/idea${IJ_BUILD}.tar.gz
   tar zxf idea${IJ_BUILD}.tar.gz
   rm -rf idea${IJ_BUILD}.tar.gz
   UNPACKED_IDEA=$(find . -name 'idea-I*' | head -n 1)
@@ -30,10 +35,15 @@ if [ ! -d .cache/intellij/plugins ]; then
   mkdir -p plugins
   pushd plugins
 
-  wget -O Scala.zip "https://plugins.jetbrains.com/pluginManager/?action=download&id=$SCALA_PLUGIN_ID&build=$FULL_IJ_BUILD_NUMBER"
+  URL="https://plugins.jetbrains.com/pluginManager/?action=download&id=${SCALA_PLUGIN_ID}&build=${FULL_IJ_BUILD_NUMBER}"
+  echo "Downloading ${URL} to" `pwd`
+  curl -L -o Scala.zip ${URL}
   unzip Scala.zip
   rm -rf Scala.zip
-  wget -O python.zip "https://plugins.jetbrains.com/pluginManager/?action=download&id=$PYTHON_PLUGIN_ID&build=$FULL_IJ_BUILD_NUMBER"
+
+  URL="https://plugins.jetbrains.com/pluginManager/?action=download&id=${PYTHON_PLUGIN_ID}&build=${FULL_IJ_BUILD_NUMBER}"
+  echo "Downloading ${URL} to" `pwd`
+  curl -L -o python.zip ${URL}
   unzip python.zip
   rm -rf python.zip
 
