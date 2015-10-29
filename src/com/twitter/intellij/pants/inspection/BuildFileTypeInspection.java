24349ae73d6a7a8f95890e943751a107e5ed96b5
wisechengyi <wisechengyi@gmail.com>
2015-10-29 15:19:18 -0700

    file type inspection

---
+++ src/com/twitter/intellij/pants/inspection/BuildFileTypeInspection.java
@@ -0,0 +1,42 @@
+// Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+package com.twitter.intellij.pants.inspection;
+
+import com.intellij.codeInspection.*;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.psi.PsiFile;
+import com.jetbrains.python.PythonFileType;
+import com.twitter.intellij.pants.PantsBundle;
+import com.twitter.intellij.pants.quickfix.TypeAssociationFix;
+import com.twitter.intellij.pants.util.PantsUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class BuildFileTypeInspection extends LocalInspectionTool {
+  @Override
+  @Nullable
+  public ProblemDescriptor[] checkFile(@NotNull PsiFile file, @NotNull InspectionManager manager, boolean isOnTheFly) {
+    if (PantsUtil.isBUILDFileName(file.getName())
+        && PantsUtil.isPythonAvailable()
+        && PantsUtil.isPantsProject(file.getProject())
+      ) {
+      FileTypeManager mgr = FileTypeManager.getInstance();
+      FileType type = mgr.getFileTypeByFileName(file.getName());
+
+      if (type != PythonFileType.INSTANCE) {
+        LocalQuickFix[] fixes = new LocalQuickFix[]{new TypeAssociationFix()};
+        ProblemDescriptor descriptor = manager.createProblemDescriptor(
+          file.getNavigationElement(),
+          PantsBundle.message("pants.info.mistreated.build.file"),
+          isOnTheFly,
+          fixes,
+          ProblemHighlightType.GENERIC_ERROR_OR_WARNING
+        );
+        return new ProblemDescriptor[]{descriptor};
+      }
+    }
+    return null;
+  }
+}
