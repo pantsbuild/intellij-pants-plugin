b59e7afb8551b3c1d33b776c2b2791350f031dbc
Yi Cheng <wisechengyi@gmail.com>
2015-10-29 14:10:35 -0700

    Show a pop up to install Python Plugin if not installed
    remind user to install python plugin for pants
    
    Testing Done:
    https://travis-ci.org/wisechengyi/intellij-pants-plugin/builds/87579452
    
    Reviewed at https://rbcommons.com/s/twitter/r/3043/

---
+++ src/com/twitter/intellij/pants/inspection/PythonPluginInspection.java
@@ -0,0 +1,31 @@
+// Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+package com.twitter.intellij.pants.inspection;
+
+import com.intellij.codeInspection.*;
+import com.intellij.psi.PsiFile;
+import com.twitter.intellij.pants.PantsBundle;
+import com.twitter.intellij.pants.quickfix.AddPythonPluginQuickFix;
+import com.twitter.intellij.pants.util.PantsUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class PythonPluginInspection extends LocalInspectionTool {
+  @Override
+  @Nullable
+  public ProblemDescriptor[] checkFile(@NotNull PsiFile file, @NotNull InspectionManager manager, boolean isOnTheFly) {
+    if (PantsUtil.isBUILDFileName(file.getName()) && !PantsUtil.isPythonAvailable() && PantsUtil.isPantsProject(file.getProject())) {
+      LocalQuickFix[] fixes = new LocalQuickFix[]{new AddPythonPluginQuickFix()};
+      ProblemDescriptor descriptor = manager.createProblemDescriptor(
+        file.getNavigationElement(),
+        PantsBundle.message("pants.info.python.plugin.missing"),
+        isOnTheFly,
+        fixes,
+        ProblemHighlightType.GENERIC_ERROR_OR_WARNING
+      );
+      return new ProblemDescriptor[]{descriptor};
+    }
+    return null;
+  }
+}
