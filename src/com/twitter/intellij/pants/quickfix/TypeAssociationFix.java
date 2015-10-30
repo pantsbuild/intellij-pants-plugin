ce57efc6bf3290bd53978ec3e77cf866935bba06
Yi Cheng <wisechengyi@gmail.com>
2015-10-30 14:41:17 -0700

    build file type inspection, and quick fix to treat build files as python files
    Testing Done:
    https://travis-ci.org/wisechengyi/intellij-pants-plugin/builds/88227056
    
    Reviewed at https://rbcommons.com/s/twitter/r/3055/

---
+++ src/com/twitter/intellij/pants/quickfix/TypeAssociationFix.java
@@ -0,0 +1,57 @@
+// Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+package com.twitter.intellij.pants.quickfix;
+
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileTypes.FileNameMatcher;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.IncorrectOperationException;
+import com.jetbrains.python.PythonFileType;
+import com.twitter.intellij.pants.PantsBundle;
+import org.jetbrains.annotations.NotNull;
+
+public class TypeAssociationFix extends PantsQuickFix {
+  @NotNull
+  @Override
+  public String getName() {
+    return PantsBundle.message("quick.fix.add.build.file.treatment");
+  }
+
+  @Override
+  public boolean startInWriteAction() {
+    return true;
+  }
+
+  @NotNull
+  @Override
+  public String getText() {
+    return PantsBundle.message("quick.fix.add.build.file.treatment");
+  }
+
+  @Override
+  public boolean isAvailable(Project project, Editor editor, PsiFile file) {
+    return true;
+  }
+
+  @Override
+  public void applyFix(Project project, ProblemDescriptor descriptor) {
+    invoke(project, null, descriptor.getPsiElement().getContainingFile());
+  }
+
+  @Override
+  public void invoke(final Project project, Editor editor, PsiFile psiFile) throws IncorrectOperationException {
+    FileTypeManager manager = FileTypeManager.getInstance();
+    FileType type = manager.getFileTypeByFileName(psiFile.getName());
+    // Remove the BUILD file matcher from the wrong type then add it to PythonFileType
+    for (FileNameMatcher matcher : manager.getAssociations(type)) {
+      if (matcher.accept(psiFile.getName())) {
+        manager.removeAssociation(type, matcher);
+      }
+    }
+  }
+}
