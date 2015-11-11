3558c4ca44efc127a2ed5f431d43156e4c8de558
Fedor Korotkov <fkorotkov@twitter.com>
2015-11-11 14:12:08 -0800

    bundle jps-plugin-system.jar within sdk jar

--- scripts/build_test_library.sh
+++ scripts/build_test_library.sh
@@ -2,6 +2,7 @@
 
 source scripts/prepare-ci-environment.sh
 
-cp $INTELLIJ_HOME/lib/rt/jps-plugin-system.jar dist/intellij-jps-plugin-system.jar
-
+# pack intellij-jps-plugin-system.jar within the sdk
+cp $INTELLIJ_HOME/lib/rt/jps-plugin-system.jar scripts/sdk/intellij-jps-plugin-system.jar
 ./pants binary scripts/sdk:
+rm scripts/sdk/intellij-jps-plugin-system.jar
