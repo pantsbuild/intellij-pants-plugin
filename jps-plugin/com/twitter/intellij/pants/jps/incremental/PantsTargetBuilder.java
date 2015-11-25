957680a24b7b562dcf58e5a26d4293e6e02b2c6c
wisechengyi <wisechengyi@gmail.com>
2015-11-24 20:03:04 -0800

    ws

--- jps-plugin/com/twitter/intellij/pants/jps/incremental/PantsTargetBuilder.java
+++ jps-plugin/com/twitter/intellij/pants/jps/incremental/PantsTargetBuilder.java
@@ -1,7 +1,7 @@
 // Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
 // Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-package com.twitter.intellij.pants.jps.incremental;
+package com.twitter.intellij.pants.jps.incremental; 
 
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.GeneralCommandLine;
