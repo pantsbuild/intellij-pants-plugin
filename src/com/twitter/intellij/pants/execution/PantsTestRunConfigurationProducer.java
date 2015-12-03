36c1eaf0f457a2b03a277cda1d7368786abd1067
wisechengyi <wisechengyi@gmail.com>
2015-12-03 10:37:35 -0800

    nifty change

--- src/com/twitter/intellij/pants/execution/PantsTestRunConfigurationProducer.java
+++ src/com/twitter/intellij/pants/execution/PantsTestRunConfigurationProducer.java
@@ -8,21 +8,19 @@ import com.intellij.execution.actions.RunConfigurationProducer;
 import com.intellij.openapi.externalSystem.model.execution.ExternalSystemTaskExecutionSettings;
 import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunConfiguration;
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
 import com.intellij.testIntegration.TestIntegrationUtils;
 import com.intellij.util.ObjectUtils;
-import com.intellij.util.containers.ContainerUtil;
 import com.twitter.intellij.pants.model.PantsTargetAddress;
 import com.twitter.intellij.pants.util.PantsConstants;
 import com.twitter.intellij.pants.util.PantsUtil;
 import org.jetbrains.annotations.NotNull;
-import com.intellij.psi.PsiMethod;
 
 import java.util.Collections;
 import java.util.List;
@@ -96,10 +94,10 @@ public class PantsTestRunConfigurationProducer extends RunConfigurationProducer<
           break;
         }
       }
-      String testFullyQualfiedName = psiClass.getQualifiedName();
+      String testFullyQualifiedName = psiClass.getQualifiedName();
       if (psiMethod != null) {
         sourceElement.set(psiMethod);
-        testFullyQualfiedName += "#" + psiMethod.getName();
+        testFullyQualifiedName += "#" + psiMethod.getName();
         configuration.setName(psiMethod.getName());
       }
       else {
@@ -109,7 +107,7 @@ public class PantsTestRunConfigurationProducer extends RunConfigurationProducer<
 
       taskSettings.setScriptParameters(
         "--no-test-junit-suppress-output " +
-        "--test-junit-test=" + testFullyQualfiedName
+        "--test-junit-test=" + testFullyQualifiedName
       );
     }
     else {
