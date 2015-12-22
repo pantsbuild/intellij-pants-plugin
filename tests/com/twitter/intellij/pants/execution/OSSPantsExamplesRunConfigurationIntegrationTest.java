f6b59f279e6ad162a84c48f84db5b130877dcd43
wisechengyi <wisechengyi@gmail.com>
2015-12-21 17:13:59 -0800

    added test for run configurations

---
+++ tests/com/twitter/intellij/pants/execution/OSSPantsExamplesRunConfigurationIntegrationTest.java
@@ -0,0 +1,118 @@
+// Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+package com.twitter.intellij.pants.execution;
+
+import com.intellij.execution.Location;
+import com.intellij.execution.PsiLocation;
+import com.intellij.execution.actions.ConfigurationContext;
+import com.intellij.execution.actions.ConfigurationFromContext;
+import com.intellij.execution.actions.RunConfigurationProducer;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunConfiguration;
+import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.psi.*;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.testFramework.MapDataContext;
+import com.twitter.intellij.pants.testFramework.OSSPantsIntegrationTest;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class OSSPantsExamplesRunConfigurationIntegrationTest extends OSSPantsIntegrationTest {
+  public void testClassRunConfiguration() throws Throwable {
+    importHello();
+
+    String classReference = "org.pantsbuild.example.hello.greet.GreetingTest";
+    PsiClass testClass = JavaPsiFacade.getInstance(myProject).findClass(classReference, GlobalSearchScope.allScope(myProject));
+    assertNotNull(testClass);
+
+    ExternalSystemRunConfiguration esc = getExternalSystemRunConfiguration(testClass);
+
+    // esc.getSettings().getScriptParameters() will return something like:
+    // "--no-test-junit-suppress-output --test-junit-test=org.pantsbuild.example.hello.greet.GreetingTest"
+    Set<String> items = new HashSet<String>(Arrays.asList(esc.getSettings().getScriptParameters().split(" ")));
+    assertTrue(items.contains("--test-junit-test=" + classReference));
+  }
+
+  public void testMethodRunConfiguration() throws Throwable {
+    importHello();
+    String classReference = "org.pantsbuild.example.hello.greet.GreetingTest";
+    String methodName = "mentionGreetee";
+
+    PsiClass testClass = JavaPsiFacade.getInstance(myProject).findClass(classReference, GlobalSearchScope.allScope(myProject));
+    PsiMethod[] testMethods = testClass.findMethodsByName(methodName, false);
+    assertEquals(testMethods.length, 1);
+    PsiMethod testMethod = testMethods[0];
+    assertNotNull(testMethod);
+
+    ExternalSystemRunConfiguration esc = getExternalSystemRunConfiguration(testMethod);
+
+    Set<String> items = new HashSet<String>(Arrays.asList(esc.getSettings().getScriptParameters().split(" ")));
+    assertTrue(items.contains("--test-junit-test=" + classReference + "#" + methodName));
+  }
+
+  private void importHello() {
+    doImport("examples/tests/java/org/pantsbuild/example/hello");
+
+    assertModules(
+      "examples_src_resources_org_pantsbuild_example_hello_hello",
+      "examples_src_java_org_pantsbuild_example_hello_greet_greet",
+      "examples_tests_java_org_pantsbuild_example_hello_greet_greet",
+      "examples_tests_java_org_pantsbuild_example_hello_module"
+    );
+  }
+
+  public void testModuleRunConfiguration() throws Throwable {
+    doImport("examples/tests/java/org/pantsbuild/example/useantlr");
+
+    assertModules(
+      "_examples_src_antlr__common_sources",
+      "examples_src_java_org_pantsbuild_example_antlr3_antlr3",
+      "_examples_tests_java_org_pantsbuild_example_useantlr_common_sources",
+      "examples_src_java_org_pantsbuild_example_antlr4_antlr4",
+      "examples_src_antlr_org_pantsbuild_example_exp_exp_antlr3",
+      "examples_src_antlr_org_pantsbuild_example_exp_exp_antlr4",
+      "examples_tests_java_org_pantsbuild_example_useantlr_antlr3_test",
+      "examples_tests_java_org_pantsbuild_example_useantlr_antlr4_test"
+    );
+
+    PsiPackage testPackage = JavaPsiFacade.getInstance(myProject).findPackage("org.pantsbuild.example.useantlr");
+    assertEquals(testPackage.getDirectories().length, 1);
+
+    ExternalSystemRunConfiguration esc = getExternalSystemRunConfiguration(testPackage.getDirectories()[0]);
+
+    Set<String> items = new HashSet<String>(Arrays.asList(esc.getSettings().getScriptParameters().split(" ")));
+    assertTrue(items.contains("--test-junit-test=org.pantsbuild.example.useantlr.Antlr3Test"));
+    assertTrue(items.contains("--test-junit-test=org.pantsbuild.example.useantlr.Antlr4Test"));
+  }
+
+  @NotNull
+  private ExternalSystemRunConfiguration getExternalSystemRunConfiguration(PsiElement psiElement) {
+    ConfigurationContext context = createContext(psiElement, new MapDataContext());
+    ConfigurationFromContext myPantsConfigurationFromContext = getPantsConfigurationFromContext(context);
+    return (ExternalSystemRunConfiguration)myPantsConfigurationFromContext.getConfiguration();
+  }
+
+  private ConfigurationFromContext getPantsConfigurationFromContext(ConfigurationContext context) {
+    for (RunConfigurationProducer producer : RunConfigurationProducer.getProducers(myProject)) {
+      if (producer instanceof PantsTestRunConfigurationProducer) {
+        return producer.createConfigurationFromContext(context);
+      }
+    }
+    return null;
+  }
+
+  private ConfigurationContext createContext(@NotNull PsiElement psiClass, @NotNull MapDataContext dataContext) {
+    dataContext.put(CommonDataKeys.PROJECT, myProject);
+    if (LangDataKeys.MODULE.getData(dataContext) == null) {
+      dataContext.put(LangDataKeys.MODULE, ModuleUtilCore.findModuleForPsiElement(psiClass));
+    }
+    dataContext.put(Location.DATA_KEY, PsiLocation.fromPsiElement(psiClass));
+    return ConfigurationContext.getFromContext(dataContext);
+  }
+}
