25f1da25d4fba4b11cefb983a82575d8ca85317d
wisechengyi <wisechengyi@gmail.com>
2015-12-23 00:01:43 -0800

    ws

--- src/com/twitter/intellij/pants/components/impl/PantsInitComponentImpl.java
+++ src/com/twitter/intellij/pants/components/impl/PantsInitComponentImpl.java
@@ -40,7 +40,7 @@ public class PantsInitComponentImpl implements PantsInitComponent {
     if (StringUtil.isNotEmpty(basePath) && plugin instanceof IdeaPluginDescriptorImpl) {
       ((IdeaPluginDescriptorImpl)plugin).setPath(new File(basePath));
     }
-    
+
     // Add (Cmd Shift R) as shortcut to refresh the project if there is no shortcut for that action yet.
     if (KeymapManager.getInstance().getActiveKeymap().getShortcuts("ExternalSystem.RefreshAllProjects").length == 0) {
       KeymapManager.getInstance().getActiveKeymap()
