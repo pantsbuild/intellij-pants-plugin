71819b57a6361f592cff3641b71b1289f4a84316
wisechengyi <wisechengyi@gmail.com>
2016-01-16 00:55:23 -0800

    root *

--- testFramework/com/twitter/intellij/pants/testFramework/PantsIntegrationTestCase.java
+++ testFramework/com/twitter/intellij/pants/testFramework/PantsIntegrationTestCase.java
@@ -87,8 +87,7 @@ public abstract class PantsIntegrationTestCase extends ExternalSystemImportingTe
   @Override
   public void setUp() throws Exception {
     super.setUp();
-    VfsRootAccess.allowRootAccess(myRelativeProjectPath);
-    VfsRootAccess.allowRootAccess("hello/im/yolo");
+    VfsRootAccess.allowRootAccess("*");
     for (String pluginId : getRequiredPluginIds()) {
       final IdeaPluginDescriptor plugin = PluginManager.getPlugin(PluginId.getId(pluginId));
       assertNotNull(pluginId + " plugin should be in classpath for integration tests!", plugin);
