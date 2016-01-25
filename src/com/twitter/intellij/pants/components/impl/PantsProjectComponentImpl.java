5c8f6e74e808423a985bf0370e459aa4125d929f
wisechengyi <wisechengyi@gmail.com>
2016-01-25 10:45:46 -0800

    Revert "to be reset"
    This reverts commit 5243d3b559d77316d3223ea4b3b3329b26479a6e.

--- src/com/twitter/intellij/pants/components/impl/PantsProjectComponentImpl.java
+++ src/com/twitter/intellij/pants/components/impl/PantsProjectComponentImpl.java
@@ -16,13 +16,6 @@ import com.twitter.intellij.pants.settings.PantsSettings;
 import com.twitter.intellij.pants.util.PantsConstants;
 import com.twitter.intellij.pants.util.PantsUtil;
 import icons.PantsIcons;
-import com.intellij.openapi.vcs.VcsConfiguration;
-import com.intellij.openapi.vfs.VirtualFile;
-import java.util.ArrayList;
-import java.util.Arrays;
-import com.intellij.openapi.vcs.ProjectLevelVcsManager;
-import com.intellij.openapi.updateSettings.impl.UpdateChecker;
-
 
 public class PantsProjectComponentImpl extends AbstractProjectComponent implements PantsProjectComponent {
   protected PantsProjectComponentImpl(Project project) {
@@ -53,15 +46,6 @@ public class PantsProjectComponentImpl extends AbstractProjectComponent implemen
               PantsUtil.refreshAllProjects(myProject);
             }
           }
-
-
-          //Find the .idea folder
-          VirtualFile dir = myProject.getBaseDir();
-          while (dir.getParent().getPath().contains(".idea")) {
-            dir = dir.getParent();
-          }
-          VcsConfiguration.getInstance(myProject).addIgnoredUnregisteredRoots(Arrays.asList(dir.getPath()));
-          //ProjectLevelVcsManager.getInstance(myProject).get
         }
       }
     );
