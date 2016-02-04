37c60efd38a71108a4c2f04d4bcff340c189e20a
wisechengyi <wisechengyi@gmail.com>
2016-02-04 15:02:09 -0800

    file

--- travis_yaml_to_bash.py
+++ travis_yaml_to_bash.py
@@ -17,7 +17,6 @@ case "${CI_SHARD}" in
   exit 1
 '''
 
-
 start = '''case "${CI_SHARD}" in'''
 end = '''esac'''
 default = '''\
@@ -25,22 +24,21 @@ default = '''\
   exit 1'''
 
 script='''\
+echo $IJ_ULTIMATE
+echo PANTS_SHA
 ./scripts/setup-ci-environment.sh
 ./scripts/run-tests-ci.sh
 '''
 components=[start]
 with open(".travis.yml", 'r') as stream:
     yml = yaml.load(stream)
-    # pprint.pprint(yml)
     for i, line in enumerate(yml['env']['matrix']):
         components.append("{}) {} \n ;;".format(i, line))
-        # print line
     components.append(default)
     components.append(end)
     components.append(script)
     final_output = '\n'.join(components)
-    print(final_output)
-    # with open(sys.argv[1], 'w') as f:
-    #     f.write(final_output)
+    with open(sys.argv[1], 'w') as f:
+        f.write(final_output)
 
 
