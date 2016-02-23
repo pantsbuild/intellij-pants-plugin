9a46326d02d6ea15ccb82aaa1337eebe81310cd1
wisechengyi <wisechengyi@gmail.com>
2016-02-22 19:04:57 -0800

    reset

--- src/com/twitter/intellij/pants/compiler/PantsBuildTargetScopeProvider.java
+++ src/com/twitter/intellij/pants/compiler/PantsBuildTargetScopeProvider.java
@@ -4,15 +4,19 @@
 package com.twitter.intellij.pants.compiler;
 
 import com.intellij.compiler.impl.BuildTargetScopeProvider;
+import com.intellij.execution.junit.JUnitConfiguration;
 import com.intellij.openapi.compiler.CompileScope;
 import com.intellij.openapi.compiler.CompilerFilter;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Key;
 import com.twitter.intellij.pants.jps.incremental.model.PantsBuildTargetType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.api.CmdlineRemoteProto.Message.ControllerMessage.ParametersMessage.TargetTypeBuildScope;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class PantsBuildTargetScopeProvider extends BuildTargetScopeProvider {
   @NotNull
@@ -25,9 +29,21 @@ public class PantsBuildTargetScopeProvider extends BuildTargetScopeProvider {
   ) {
     final TargetTypeBuildScope.Builder builder =
       TargetTypeBuildScope.newBuilder()
-      .setTypeId(PantsBuildTargetType.INSTANCE.getTypeId())
-      .setAllTargets(true)
-      .setForceBuild(forceBuild);
+        .setTypeId(PantsBuildTargetType.INSTANCE.getTypeId())
+        .setAllTargets(false)
+        .setForceBuild(forceBuild);
+
+    for (Map.Entry<Key, Object> entry : baseScope.exportUserData().entrySet()) {
+      if (entry.getKey().toString().equals("RUN_CONFIGURATION")) {
+        if (entry.getValue() instanceof  JUnitConfiguration){
+          JUnitConfiguration config = (JUnitConfiguration) entry.getValue();
+          Module[] targetModules = config.getModules();
+          for (int i = 0; i < targetModules.length; i++) {
+            builder.addTargetId(targetModules[i].getName());
+          }
+        }
+      }
+    }
     return Collections.singletonList(builder.build());
   }
 }
