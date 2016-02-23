153e6090d1bf63ca00f09708c4125a183c564017
Yi Cheng <wisechengyi@gmail.com>
2016-02-23 10:54:42 -0800

    Exclude all from scala plugin compilation
    Some user hit the issue that after pants finishes, scala plugin compiler builder also started to build and cause error. Since all comiplation can/should be handled by pants, we should completely block scala plugin from compiling our code.
    
    Testing Done:
    https://travis-ci.org/wisechengyi/intellij-pants-plugin/builds/110483748
    
    Reviewed at https://rbcommons.com/s/twitter/r/3483/

--- jps-plugin/com/twitter/intellij/pants/jps/incremental/scala/PantsScalaChunkExclusionService.java
+++ jps-plugin/com/twitter/intellij/pants/jps/incremental/scala/PantsScalaChunkExclusionService.java
@@ -3,28 +3,13 @@
 
 package com.twitter.intellij.pants.jps.incremental.scala;
 
-import com.twitter.intellij.pants.jps.incremental.model.JpsPantsProjectExtension;
-import com.twitter.intellij.pants.jps.incremental.serialization.PantsJpsProjectExtensionSerializer;
-import com.twitter.intellij.pants.jps.util.PantsJpsUtil;
 import org.jetbrains.jps.ModuleChunk;
 import org.jetbrains.jps.incremental.scala.ChunkExclusionService;
-import org.jetbrains.jps.model.module.JpsModule;
-
-import java.util.Set;
 
 public class PantsScalaChunkExclusionService extends ChunkExclusionService {
   @Override
   public boolean isExcluded(ModuleChunk chunk) {
-    final Set<JpsModule> modules = chunk.getModules();
-    if (modules.isEmpty()) {
-      return false;
-    }
-
-    final JpsPantsProjectExtension pantsProjectExtension =
-      PantsJpsProjectExtensionSerializer.findPantsProjectExtension(modules.iterator().next().getProject());
-    if (pantsProjectExtension == null) {
-      return false;
-    }
-    return PantsJpsUtil.containsPantsModules(modules);
+    // All compilations are handled by Pants, thus excluded from scala plugin.
+    return true;
   }
 }
