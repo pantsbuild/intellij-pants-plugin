096bc89909c52be3ed93c3d20094729ebcc6e531
wisechengyi <wisechengyi@gmail.com>
2016-03-09 16:18:11 -0800

    test fix

--- tests/com/twitter/intellij/pants/integration/OSSPantsTestExamplesIntegrationTest.java
+++ tests/com/twitter/intellij/pants/integration/OSSPantsTestExamplesIntegrationTest.java
@@ -21,7 +21,7 @@ public class OSSPantsTestExamplesIntegrationTest extends OSSPantsIntegrationTest
     assertModules("intellij-integration_tests_java_org_pantsbuild_testprojects_testprojects");
 
     List<String> output = makeModules("intellij-integration_tests_java_org_pantsbuild_testprojects_testprojects");
-    assertContainsSubstring(output, "compile intellij-integration/tests/java/org/pantsbuild/testprojects:testprojects --no");
+    assertContainsSubstring(output, "compile intellij-integration/tests/java/org/pantsbuild/testprojects:testprojects");
     assertSuccessfulJUnitTest(
       "intellij-integration_tests_java_org_pantsbuild_testprojects_testprojects", "org.pantsbuild.testprojects.JUnitIntegrationTest");
     final OSProcessHandler processHandler = runJUnitTest(
@@ -41,13 +41,13 @@ public class OSSPantsTestExamplesIntegrationTest extends OSSPantsIntegrationTest
     doImport("testprojects/tests/java/org/pantsbuild/testproject/matcher");
 
     List<String> output = makeModules("testprojects_tests_java_org_pantsbuild_testproject_matcher_matcher");
-    assertContainsSubstring(output, "compile testprojects/tests/java/org/pantsbuild/testproject/matcher:matcher --no");
+    assertContainsSubstring(output, "compile testprojects/tests/java/org/pantsbuild/testproject/matcher:matcher");
     assertSuccessfulJUnitTest(
       "testprojects_tests_java_org_pantsbuild_testproject_matcher_matcher", "org.pantsbuild.testproject.matcher.MatcherTest");
 
     // _testprojects_tests_java_org_pantsbuild_testproject_dummies_common_sources does not have a corresponding target address.
     // Thus compile all 3 targets. 2 in dummies and 1 in matcher.
     List<String> output2 = makeModules("_testprojects_tests_java_org_pantsbuild_testproject_dummies_common_sources");
-    assertContainsSubstring(output2, "Compiling 3 targets");
+    assertContainsSubstring(output2, "Compiling 2 targets");
   }
 }
