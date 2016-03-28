cdf66698472290a603910d444d50172442d641c8
wisechengyi <wisechengyi@gmail.com>
2016-03-28 16:50:31 -0700

    more cleanup

--- tests/com/twitter/intellij/pants/integration/OSSPantsMakeIntegrationTest.java
+++ tests/com/twitter/intellij/pants/integration/OSSPantsMakeIntegrationTest.java
@@ -5,43 +5,31 @@ package com.twitter.intellij.pants.integration;
 
 import com.intellij.execution.BeforeRunTask;
 import com.intellij.execution.BeforeRunTaskProvider;
-import com.intellij.execution.ExecutionManager;
 import com.intellij.execution.RunManager;
 import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.execution.impl.RunManagerImpl;
 import com.intellij.execution.junit.JUnitConfiguration;
 import com.intellij.execution.junit.JUnitConfigurationType;
-import com.intellij.openapi.externalSystem.service.execution.ExternalSystemBeforeRunTask;
 import com.twitter.intellij.pants.execution.PantsMakeBeforeRun;
 import com.twitter.intellij.pants.testFramework.OSSPantsIntegrationTest;
-import com.twitter.intellij.pants.util.PantsConstants;
 
-import java.util.Collections;
 import java.util.List;
 
 
 public class OSSPantsMakeIntegrationTest extends OSSPantsIntegrationTest {
   public void testPantsMake() throws Throwable {
-    doImport("testprojects/tests/java/org/pantsbuild/testproject/matcher");
-    //doImport("testprojects/tests/java/org/pantsbuild/testproject/dummies");
+    doImport("testprojects/tests/java/org/pantsbuild/testproject/");
 
     RunManager runManager = RunManager.getInstance(myProject);
-    if (!(runManager instanceof RunManagerImpl)) {
-      return;
-    }
+    assertTrue(runManager instanceof RunManagerImpl);
+
     RunManagerImpl runManagerImpl = (RunManagerImpl) runManager;
-
-    //BeforeRunTask pantsMakeTask = new ExternalSystemBeforeRunTask(PantsMakeBeforeRun.ID, PantsConstants.SYSTEM_ID);
-    //pantsMakeTask.setEnabled(true);
-
     JUnitConfiguration runConfiguration = generateJUnitConfiguration(
       "testprojects_tests_java_org_pantsbuild_testproject_matcher_matcher", "org.pantsbuild.testproject.matcher.MatcherTest", null);
 
-    //runManagerImpl.setBeforeRunTasks(runConfiguration, Collections.singletonList(pantsMakeTask), false);
-
     RunnerAndConfigurationSettings runnerAndConfigurationSettings =
-      runManagerImpl.createConfiguration(runConfiguration, JUnitConfigurationType.getInstance().getConfigurationFactories()[0]);
-    runManagerImpl.addConfiguration(runnerAndConfigurationSettings, false);
+      runManager.createConfiguration(runConfiguration, JUnitConfigurationType.getInstance().getConfigurationFactories()[0]);
+    runManager.addConfiguration(runnerAndConfigurationSettings, false);
 
     // Make sure PantsMake is the one and only task before JUnit run.
     List<BeforeRunTask> beforeRunTaskList = runManagerImpl.getBeforeRunTasks(runConfiguration);
