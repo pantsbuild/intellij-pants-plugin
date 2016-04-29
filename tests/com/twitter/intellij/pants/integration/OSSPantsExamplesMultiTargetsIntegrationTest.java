ff5fe839942d08646985a0021dd70c8bada56960
wisechengyi <wisechengyi@gmail.com>
2016-04-28 22:44:43 -0700

    make modules are stale

--- tests/com/twitter/intellij/pants/integration/OSSPantsExamplesMultiTargetsIntegrationTest.java
+++ tests/com/twitter/intellij/pants/integration/OSSPantsExamplesMultiTargetsIntegrationTest.java
@@ -3,13 +3,11 @@
 
 package com.twitter.intellij.pants.integration;
 
-import com.twitter.intellij.pants.settings.PantsSettings;
 import com.twitter.intellij.pants.testFramework.OSSPantsIntegrationTest;
 
 public class OSSPantsExamplesMultiTargetsIntegrationTest extends OSSPantsIntegrationTest {
   public void testHello() throws Throwable {
     doImport("examples/src/java/org/pantsbuild/example/hello");
-
     assertModules(
       "examples_src_resources_org_pantsbuild_example_hello_hello",
       "examples_src_java_org_pantsbuild_example_hello_main_main",
@@ -19,13 +17,7 @@ public class OSSPantsExamplesMultiTargetsIntegrationTest extends OSSPantsIntegra
       "examples_src_java_org_pantsbuild_example_hello_module"
     );
 
-    makeModules("examples_src_java_org_pantsbuild_example_hello_main_main");
-    assertClassFileInModuleOutput(
-      "org.pantsbuild.example.hello.greet.Greeting", "examples_src_java_org_pantsbuild_example_hello_greet_greet"
-    );
-
     doImport("examples/src/scala/org/pantsbuild/example/hello/BUILD", "hello");
-
     assertModules(
       "examples_src_resources_org_pantsbuild_example_hello_hello",
       "examples_src_java_org_pantsbuild_example_hello_main_main",
@@ -38,9 +30,7 @@ public class OSSPantsExamplesMultiTargetsIntegrationTest extends OSSPantsIntegra
       "examples_src_scala_org_pantsbuild_example_hello_welcome_welcome",
       "examples_src_scala_org_pantsbuild_example_hello_exe_exe"
     );
-
     assertContainsSubstring(makeProject(), "Compiling 8 targets");
-
     assertClassFileInModuleOutput(
       "org.pantsbuild.example.hello.welcome.WelcomeEverybody", "examples_src_scala_org_pantsbuild_example_hello_welcome_welcome"
     );
