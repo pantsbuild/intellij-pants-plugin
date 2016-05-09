86af8351c2274e5aef55179a7ba297d7a13b801b
wisechengyi <wisechengyi@gmail.com>
2016-05-09 13:53:03 -0700

    comments

--- src/com/twitter/intellij/pants/service/project/PantsSystemProjectResolver.java
+++ src/com/twitter/intellij/pants/service/project/PantsSystemProjectResolver.java
@@ -24,11 +24,9 @@ import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotifica
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListener;
 import com.intellij.openapi.externalSystem.service.project.ExternalSystemProjectResolver;
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
-import com.intellij.openapi.externalSystem.util.ExternalSystemUtil;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.module.ModuleTypeId;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -221,9 +219,7 @@ public class PantsSystemProjectResolver implements ExternalSystemProjectResolver
 
     public void asyncViewSwitch() {
       /**
-       * Ensure GUI is set correctly because the seed(empty) project does not set it:
        * 1. Make sure the project view opened so the view switch will follow.
-       * 2. Pants tool window is initialized; otherwise no message is shown when invoking `PantsCompile`.
        */
       if (!ApplicationManager.getApplication().isUnitTestMode()) {
         if (ToolWindowManager.getInstance(myProject).getToolWindow("Project") != null) {
