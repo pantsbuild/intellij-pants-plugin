fcc833e1e325945cc9a5ff9ee9dbaf6372af4ddc
Yi Cheng <wisechengyi@gmail.com>
2016-05-16 14:20:15 -0700

    [plugin] Trigger async file synchronization after project refreshes.
    If a thrift file is changed and `pants gen` was called, IDEA does not immediately reflect the change in the generated source file until an explicit user action 'file -> synchronize'.
    
    This RB will trigger file sync after:
    
    * project refresh.
    * external pants compile.
    * PantsMakeBeforeRun.
    * pants logo run.
    
    Testing Done:
    https://travis-ci.org/pantsbuild/intellij-pants-plugin/builds/130318076
    
    + Manual testing to confirm thrift changes are reflected under the following circumstances:
    
    * project refresh.
    * external pants compile.
    * PantsMakeBeforeRun.
    * pants logo run.
    
    Bugs closed: 142
    
    Reviewed at https://rbcommons.com/s/twitter/r/3854/

--- src/com/twitter/intellij/pants/service/task/PantsTaskManager.java
+++ src/com/twitter/intellij/pants/service/task/PantsTaskManager.java
@@ -132,6 +132,8 @@ public class PantsTaskManager extends AbstractExternalSystemTaskManager<PantsExe
     }
     finally {
       myCancellationMap.remove(id);
+      // Sync files as generated sources may have changed after `pants test` called
+      PantsUtil.synchronizeFiles();
     }
   }
 
