dea19350543a3f404f7d4dc62bdf37fda840e915
wisechengyi <wisechengyi@gmail.com>
2016-06-30 14:37:04 -0700

    tweak

--- scripts/prepare-ci-environment.sh
+++ scripts/prepare-ci-environment.sh
@@ -54,6 +54,7 @@ append_intellij_jvm_options() {
     "-Didea.home.path=$IDEA_TEST_HOME"
     "-Dpants.plugin.base.path=$CWD/.pants.d/compile/jvm/java"
     "-Dpants.jps.plugin.classpath=$CWD/jps-plugin:$INTELLIJ_HOME/lib/rt/jps-plugin-system.jar:$CWD/jps-plugin/lib/gson-2.3.1.jar"
+    "-Didea.plugins.compatible.build=$IJ_BUILD_NUMBER"
     # "-Dcompiler.process.debug.port=5006"
   )
   for jvm_option in ${INTELLIJ_JVM_OPTIONS[@]}
