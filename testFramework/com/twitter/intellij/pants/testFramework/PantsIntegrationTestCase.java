22ed5365e25df4576433845b28bd111a14fda132
wisechengyi <wisechengyi@gmail.com>
2016-06-29 22:33:11 -0700

    version

--- testFramework/com/twitter/intellij/pants/testFramework/PantsIntegrationTestCase.java
+++ testFramework/com/twitter/intellij/pants/testFramework/PantsIntegrationTestCase.java
@@ -86,14 +86,13 @@ public abstract class PantsIntegrationTestCase extends ExternalSystemImportingTe
 
   @Override
   public void setUp() throws Exception {
-    super.setUp();
     VfsRootAccess.allowRootAccess("/");
+    super.setUp();
     for (String pluginId : getRequiredPluginIds()) {
       final IdeaPluginDescriptor plugin = PluginManager.getPlugin(PluginId.getId(pluginId));
       assertNotNull(pluginId + " plugin should be in classpath for integration tests!", plugin);
       assertTrue(pluginId + " is not enabled!", plugin.isEnabled());
     }
-
     myProjectSettings = new PantsProjectSettings();
     myCompilerTester = null;
   }
