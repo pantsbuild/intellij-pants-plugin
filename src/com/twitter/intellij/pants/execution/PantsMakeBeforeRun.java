bef84b8f106553696c0a6df7adc2605bb6702dfd
Yi Cheng <wisechengyi@gmail.com>
2016-07-20 17:53:10 -0700

    Various fixes for Pants Test configuration
    Earlier Pants test determines the target after user click `Run` or `Debug` by going up the directories until it finds a BUILD file, then append `::`, so
    1. User has no flexibilty selecting targets in the configuration.
    2. `::` is not always correct because e.g. `run` goal should only take one target. and Pants test configuration should be designed to handle generic pants commands.
    
    This RB fix them by moving target info explicitly into configuration's script parameters.
    
    Additional fixes:
    1. Remove `PantsCompile` from Pants Test configuration, as `PantsCompile` is intended for configuration that needs Pants' product but does not associate with Pants by default.
    2. Add `!isGenTarget(targetAdress)` filter on address to compile.
    3. Disable timeout on debug runs.
    4. More test coverage.
    
    Testing Done:
    https://travis-ci.org/pantsbuild/intellij-pants-plugin/builds/144630806
    https://travis-ci.org/pantsbuild/intellij-pants-plugin/builds/146213171
    
    Bugs closed: 181, 184
    
    Reviewed at https://rbcommons.com/s/twitter/r/4068/

--- src/com/twitter/intellij/pants/execution/PantsMakeBeforeRun.java
+++ src/com/twitter/intellij/pants/execution/PantsMakeBeforeRun.java
@@ -93,6 +93,12 @@ public class PantsMakeBeforeRun extends ExternalSystemBeforeRunTaskProvider {
         ((CommonProgramRunConfigurationParameters) runConfiguration).setWorkingDirectory(buildRoot.getPath());
       }
     }
+    /**
+     * If neither applies (e.g. Pants or pytest configuration), do not continue.
+     */
+    else {
+      return;
+    }
 
     /**
      * Every time a new configuration is created, 'Make' is by default added to the "Before launch" tasks.
@@ -282,17 +288,10 @@ public class PantsMakeBeforeRun extends ExternalSystemBeforeRunTaskProvider {
     if (targetModules.length == 0) {
       return Collections.emptySet();
     }
-
-    Set<String> result = new HashSet<String>();
-
+    Set<String> result = new HashSet<>();
     for (Module targetModule : targetModules) {
-      String dehydratedAddresses = targetModule.getOptionValue(PantsConstants.PANTS_TARGET_ADDRESSES_KEY);
-      if (dehydratedAddresses == null) {
-        continue;
-      }
-      result.addAll(PantsUtil.hydrateTargetAddresses(dehydratedAddresses));
+      result.addAll(PantsUtil.getNonGenTargetAddresses(targetModule));
     }
-
     return result;
   }
 
--- tests/com/twitter/intellij/pants/execution/OSSPantsExamplesRunConfigurationIntegrationTest.java
+++ tests/com/twitter/intellij/pants/execution/OSSPantsExamplesRunConfigurationIntegrationTest.java
@@ -8,18 +8,31 @@ import com.intellij.execution.PsiLocation;
 import com.intellij.execution.actions.ConfigurationContext;
 import com.intellij.execution.actions.ConfigurationFromContext;
 import com.intellij.execution.actions.RunConfigurationProducer;
+import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.externalSystem.ExternalSystemManager;
 import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunConfiguration;
+import com.intellij.openapi.externalSystem.task.ExternalSystemTaskManager;
 import com.intellij.openapi.module.ModuleUtilCore;
-import com.intellij.psi.*;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiPackage;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.testFramework.MapDataContext;
+import com.twitter.intellij.pants.PantsManager;
+import com.twitter.intellij.pants.service.task.PantsTaskManager;
+import com.twitter.intellij.pants.settings.PantsExecutionSettings;
 import com.twitter.intellij.pants.testFramework.OSSPantsIntegrationTest;
+import com.twitter.intellij.pants.util.PantsUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 
@@ -28,15 +41,88 @@ public class OSSPantsExamplesRunConfigurationIntegrationTest extends OSSPantsInt
     doImport("testprojects/tests/java/org/pantsbuild/testproject/testjvms");
 
     String classReference = "org.pantsbuild.testproject.testjvms.TestSix";
+
     PsiClass testClass = JavaPsiFacade.getInstance(myProject).findClass(classReference, GlobalSearchScope.allScope(myProject));
     assertNotNull(testClass);
 
     ExternalSystemRunConfiguration esc = getExternalSystemRunConfiguration(testClass);
 
-    // esc.getSettings().getScriptParameters() will return something like:
-    // "--no-colors test --test-junit-test=org.pantsbuild.testproject.testjvms.TestSix"
-    Set<String> items = new HashSet<String>(Arrays.asList(esc.getSettings().getScriptParameters().split(" ")));
-    assertTrue(items.contains("--test-junit-test=" + classReference));
+    // Make sure task name is `test` goal.
+    assertEquals(Collections.singletonList("test"), esc.getSettings().getTaskNames());
+
+    List<String> configScriptParameters = PantsUtil.parseCmdParameters(esc.getSettings().getScriptParameters());
+
+    List<String> expectedConfigScriptParameters = Arrays.asList(
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:eight-test-platform",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:six",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:seven",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:eight",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:base",
+      "--test-junit-test=" + classReference
+    );
+    assertEquals(expectedConfigScriptParameters, configScriptParameters);
+
+    // Make sure this configuration does not contain any task before running if added to the project.
+    assertEmptyBeforeRunTask(esc);
+
+    /**
+     * Check the final command line parameters when config is run, as
+     * {@link com.twitter.intellij.pants.service.task.PantsTaskManager} will insert additional parameters post user action.
+     */
+    Class<? extends ExternalSystemTaskManager<PantsExecutionSettings>> taskManagerClass =
+      ExternalSystemManager.EP_NAME.findExtension(PantsManager.class).getTaskManagerClass();
+    assertEquals(PantsTaskManager.class, taskManagerClass);
+
+    String debuggerSetup = "dummy_debugger_setup";
+
+    GeneralCommandLine finalDebugCommandline = getFinalCommandline(esc, debuggerSetup, taskManagerClass);
+
+    List<String> expectedFinalDebugCommandlineParameters = Arrays.asList(
+      "--no-colors",
+      "--no-test-junit-timeouts",
+      "--jvm-test-junit-options=" + debuggerSetup,
+      "test",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:eight-test-platform",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:six",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:seven",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:eight",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:base",
+      "--test-junit-test=" + classReference
+    );
+
+    assertEquals(expectedFinalDebugCommandlineParameters, finalDebugCommandline.getParametersList().getParameters());
+
+    GeneralCommandLine finalRunCommandline = getFinalCommandline(esc, null, taskManagerClass);
+
+    List<String> expectedFinalRunCommandlineParameters = Arrays.asList(
+      "--no-colors",
+      "test",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:eight-test-platform",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:six",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:seven",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:eight",
+      "testprojects/tests/java/org/pantsbuild/testproject/testjvms:base",
+      "--test-junit-test=" + classReference
+    );
+    assertEquals(expectedFinalRunCommandlineParameters, finalRunCommandline.getParametersList().getParameters());
+  }
+
+  @NotNull
+  private GeneralCommandLine getFinalCommandline(
+    ExternalSystemRunConfiguration esc,
+    String debuggerSetup,
+    Class<? extends ExternalSystemTaskManager<PantsExecutionSettings>> taskManagerClass
+  ) throws InstantiationException, IllegalAccessException {
+    GeneralCommandLine commandLine = ((PantsTaskManager) taskManagerClass.newInstance()).constructCommandLine(
+      esc.getSettings().getTaskNames(),
+      esc.getSettings().getExternalProjectPath(),
+      PantsExecutionSettings.createDefault(),
+      PantsUtil.parseCmdParameters(esc.getSettings().getVmOptions()),
+      PantsUtil.parseCmdParameters(esc.getSettings().getScriptParameters()),
+      debuggerSetup
+    );
+    assertNotNull(commandLine);
+    return commandLine;
   }
 
   public void testMethodRunConfiguration() throws Throwable {
@@ -62,11 +148,11 @@ public class OSSPantsExamplesRunConfigurationIntegrationTest extends OSSPantsInt
     doImport("testprojects/tests/java/org/pantsbuild/testproject/testjvms");
 
     PsiPackage testPackage = JavaPsiFacade.getInstance(myProject).findPackage("org.pantsbuild.testproject.testjvms");
-    assertEquals(testPackage.getDirectories().length, 1);
+    assertEquals(1, testPackage.getDirectories().length);
 
     ExternalSystemRunConfiguration esc = getExternalSystemRunConfiguration(testPackage.getDirectories()[0]);
 
-    Set<String> items = new HashSet<String>(Arrays.asList(esc.getSettings().getScriptParameters().split(" ")));
+    Set<String> items = new HashSet<>(Arrays.asList(esc.getSettings().getScriptParameters().split(" ")));
     assertTrue(items.contains("--test-junit-test=org.pantsbuild.testproject.testjvms.TestSix"));
     assertTrue(items.contains("--test-junit-test=org.pantsbuild.testproject.testjvms.TestSeven"));
     assertTrue(items.contains("--test-junit-test=org.pantsbuild.testproject.testjvms.TestEight"));
@@ -76,7 +162,7 @@ public class OSSPantsExamplesRunConfigurationIntegrationTest extends OSSPantsInt
   private ExternalSystemRunConfiguration getExternalSystemRunConfiguration(PsiElement psiElement) {
     ConfigurationContext context = createContext(psiElement, new MapDataContext());
     ConfigurationFromContext myPantsConfigurationFromContext = getPantsConfigurationFromContext(context);
-    return (ExternalSystemRunConfiguration)myPantsConfigurationFromContext.getConfiguration();
+    return (ExternalSystemRunConfiguration) myPantsConfigurationFromContext.getConfiguration();
   }
 
   private ConfigurationFromContext getPantsConfigurationFromContext(ConfigurationContext context) {
