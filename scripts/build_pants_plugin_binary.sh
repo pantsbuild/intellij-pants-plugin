976c268ac091713c522110ccaacaadb9d0814f27
wisechengyi <wisechengyi@gmail.com>
2016-07-26 12:01:46 -0700

    revert somet stuff

--- scripts/build_pants_plugin_binary.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source scripts/prepare-ci-environment.sh
-
-# pack intellij-jps-plugin-system.jar within the sdk
-cp $INTELLIJ_HOME/lib/rt/jps-plugin-system.jar scripts/sdk/intellij-jps-plugin-system.jar
-./pants binary scripts/sdk:pants-test-lib
-rm scripts/sdk/intellij-jps-plugin-system.jar
