d44d9e15df29af6dacef9931df9389a82a16f9c6
wisechengyi <wisechengyi@gmail.com>
2016-07-26 16:31:33 -0700

    one colon

--- scripts/run-tests-ci.sh
+++ scripts/run-tests-ci.sh
@@ -21,7 +21,7 @@ fi
 popd
 popd
 
-args="test tests:${TEST_SET:-::} $(append_intellij_jvm_options test-junit) ${ADDITIONAL_ARGS:-$@}"
+args="test tests:${TEST_SET:-:} $(append_intellij_jvm_options test-junit) ${ADDITIONAL_ARGS:-$@}"
 
 echo "Running ./pants $args"
 ./pants $args
