c6577962ffc3d8571df578ce5d6b28f04e794e8d
wisechengyi <wisechengyi@gmail.com>
2016-08-19 14:20:02 -0700

    fix

--- common/com/twitter/intellij/pants/util/PantsUtil.java
+++ common/com/twitter/intellij/pants/util/PantsUtil.java
@@ -859,7 +859,7 @@ public class PantsUtil {
 
   @Nullable
   public static File findPantsExecutable(@NotNull File file) {
-    VirtualFile vf = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(file);
+    VirtualFile vf = findPantsExecutable(LocalFileSystem.getInstance().refreshAndFindFileByIoFile(file));
     if (vf == null) {
       return null;
     }
