e9c6885ce32014db60d08dbcec112f35b3311ced
wisechengyi <wisechengyi@gmail.com>
2016-08-26 15:59:00 -0700

    move over to integration

--- tests/com/twitter/intellij/pants/integration/ResolveIntegrationTest.java
+++ tests/com/twitter/intellij/pants/integration/ResolveIntegrationTest.java
@@ -1,7 +1,7 @@
 // Copyright 2016 Pants project contributors (see CONTRIBUTORS.md).
 // Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-package com.twitter.intellij.pants.resolve;
+package com.twitter.intellij.pants.integration;
 
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.roots.ModuleRootManager;
