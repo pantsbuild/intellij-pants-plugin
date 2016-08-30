e8bdcb66525e04958e2cf9b333726f600f78380f
wisechengyi <wisechengyi@gmail.com>
2016-08-29 18:29:57 -0700

    typo

--- tests/com/twitter/intellij/pants/integration/OSSPantsExamplesMultiTargetsIntegrationTest.java
+++ tests/com/twitter/intellij/pants/integration/OSSPantsExamplesMultiTargetsIntegrationTest.java
@@ -9,7 +9,7 @@ public class OSSPantsExamplesMultiTargetsIntegrationTest extends OSSPantsIntegra
   public void testHelloJavaAndScala() throws Throwable {
     doImport("examples/src/java/org/pantsbuild/example/hello");
 
-    assertProjectName("pants_examples/src/java/org/pantsbuild/example/hello::");
+    assertProjectName("pants/examples/src/java/org/pantsbuild/example/hello::");
     assertModules(
       "examples_src_resources_org_pantsbuild_example_hello_hello",
       "examples_src_java_org_pantsbuild_example_hello_main_main",
