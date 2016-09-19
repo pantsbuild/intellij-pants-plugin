ebc5bbf2afc74815200439a8ac500df53748b50e
wisechengyi <wisechengyi@gmail.com>
2016-09-18 21:39:37 -0700

    more tests

---
+++ tests/com/twitter/intellij/pants/integration/PantsCompileOptionsExecutorTest.java
@@ -0,0 +1,34 @@
+// Copyright 2016 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+package com.twitter.intellij.pants.integration;
+
+import com.twitter.intellij.pants.service.PantsCompileOptionsExecutor;
+import com.twitter.intellij.pants.settings.PantsExecutionSettings;
+import com.twitter.intellij.pants.testFramework.OSSPantsIntegrationTest;
+
+import java.io.File;
+import java.util.Collections;
+
+public class PantsCompileOptionsExecutorTest extends OSSPantsIntegrationTest {
+
+  public void testProjectName() throws Throwable {
+    String deepDir = new String(new char[100]).replace("\0", "dummy/");
+
+    PantsExecutionSettings settings = new PantsExecutionSettings(
+      Collections.singletonList(deepDir),
+      false, // with dependees. does not matter here
+      false, // include libs and sources. does not matter here
+      false // use idea project jdk. does not matter here.
+    );
+
+    PantsCompileOptionsExecutor executor = PantsCompileOptionsExecutor.create(
+      getProjectFolder().getPath(),
+      settings
+    );
+
+    String projectName = executor.getProjectName();
+    assertNotContainsSubstring(projectName, File.separator);
+    assertEquals(PantsCompileOptionsExecutor.PROJECT_NAME_LIMIT, projectName.length());
+  }
+}
