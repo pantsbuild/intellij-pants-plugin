ce5ca2b4f2ac4b8aa811c3e3e12bbb0d4f80ae44
wisechengyi <wisechengyi@gmail.com>
2016-09-20 21:53:33 -0700

    delete more

--- jps-plugin/com/twitter/intellij/pants/jps/incremental/serialization/PantsJpsModelSerializerExtension.java
+++ jps-plugin/com/twitter/intellij/pants/jps/incremental/serialization/PantsJpsModelSerializerExtension.java
@@ -3,12 +3,7 @@
 
 package com.twitter.intellij.pants.jps.incremental.serialization;
 
-import com.google.gson.Gson;
-import com.twitter.intellij.pants.jps.incremental.model.JpsPantsModuleExtension;
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.jps.model.module.JpsModule;
 import org.jetbrains.jps.model.serialization.JpsModelSerializerExtension;
 import org.jetbrains.jps.model.serialization.JpsProjectExtensionSerializer;
 
@@ -16,16 +11,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class PantsJpsModelSerializerExtension extends JpsModelSerializerExtension {
-  // same as in ExternalSystemConstants but it is used in an external process so we can't use it directly
-  @NonNls @NotNull public static final String EXTERNAL_SYSTEM_ID_KEY = "external.system.id";
-  @NonNls @NotNull public static final String LINKED_PROJECT_PATH_KEY = "external.linked.project.path";
-  public static final Gson gson = new Gson();
-
-  @Nullable
-  public static JpsPantsModuleExtension findPantsModuleExtension(@Nullable JpsModule module) {
-    return module != null ? module.getContainer().getChild(JpsPantsModuleExtension.ROLE) : null;
-  }
-
   @NotNull
   @Override
   public List<? extends JpsProjectExtensionSerializer> getProjectExtensionSerializers() {
