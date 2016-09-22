b9d39a127cd1eff2db2cc1a81285ea593d1cdab6
Yi Cheng <wisechengyi@gmail.com>
2016-09-21 21:46:42 -0700

    Retire external builder for pants. Leave an error message in case user accidentally stumps on it.
    With `PantsCompile` as a the make step for all intellij Pants project, the legacy external builder extension for the built-in Make process is no longer needed. This should have no user impact since user already has no explicit access to the built-in Make process.
    
    Testing Done:
    https://travis-ci.org/pantsbuild/intellij-pants-plugin/builds/161712137
    
    Bugs closed: 208
    
    Reviewed at https://rbcommons.com/s/twitter/r/4252/

---
+++ tests/com/twitter/intellij/pants/integration/ExternalBuilderIntegrationTest.java
@@ -0,0 +1,31 @@
+// Copyright 2016 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+package com.twitter.intellij.pants.integration;
+
+import com.intellij.openapi.compiler.CompilerMessage;
+import com.twitter.intellij.pants.testFramework.OSSPantsIntegrationTest;
+import com.twitter.intellij.pants.util.PantsConstants;
+
+import java.util.List;
+
+public class ExternalBuilderIntegrationTest extends OSSPantsIntegrationTest {
+  public void testExternalBuilderError() throws Throwable {
+    doImport("examples/src/java/org/pantsbuild/example/hello");
+
+    assertFirstSourcePartyModules(
+      "examples_src_resources_org_pantsbuild_example_hello_hello",
+      "examples_src_java_org_pantsbuild_example_hello_main_main",
+      "examples_src_java_org_pantsbuild_example_hello_greet_greet",
+      "examples_src_java_org_pantsbuild_example_hello_simple_simple",
+      "examples_src_java_org_pantsbuild_example_hello_main_main-bin",
+      "examples_src_java_org_pantsbuild_example_hello_module"
+    );
+
+    List<CompilerMessage> make = getCompilerTester().make();
+    assertContainsSubstring(
+      make.iterator().next().getMessage(),
+      PantsConstants.EXTERNAL_BUILDER_ERROR
+    );
+  }
+}
