601feb93e17b045cdc13cc97eb240aa55d2f6ca3
wisechengyi <wisechengyi@gmail.com>
2016-11-23 14:11:50 -0800

    clarification

--- scripts/prepare-ci-environment.sh
+++ scripts/prepare-ci-environment.sh
@@ -37,7 +37,7 @@ fi
 
 # we will use Community ids to download plugins.
 export SCALA_PLUGIN_ID="org.intellij.scala"
-export SCALA_PLUGIN_MD5="6fab4fe5486d7cd22eb3164c40c1fc6c"
+export SCALA_PLUGIN_MD5="6fab4fe5486d7cd22eb3164c40c1fc6c" # 2016.3.3
 
 export INTELLIJ_PLUGINS_HOME="$CWD/.cache/intellij/$FULL_IJ_BUILD_NUMBER/plugins"
 export INTELLIJ_HOME="$CWD/.cache/intellij/$FULL_IJ_BUILD_NUMBER/idea-dist"
