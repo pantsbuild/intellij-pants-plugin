7da91fc06f40e8991b5d0b42342a54b7ba2b6af4
wisechengyi <wisechengyi@gmail.com>
2016-11-30 10:35:07 -0800

    reorg

--- scripts/get_3rdparty_jars.py
+++ scripts/get_3rdparty_jars.py
@@ -1,6 +1,6 @@
 import subprocess
 
-output = subprocess.check_output("./pants dependencies : 3rdparty/intellij:: | grep 'com\|org\|jdk'", shell=True)
+output = subprocess.check_output("./pants dependencies : 3rdparty/intellij:: | sort | grep 'com\|org\|jdk'", shell=True)
 
 for l in output.splitlines():
   org, package, _ = l.split(':')
