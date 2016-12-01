de7f99cab3ba31fcd4f2d7e0444f30fff0113a2b
wisechengyi <wisechengyi@gmail.com>
2016-11-30 23:08:33 -0800

    master only

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,2 +1,5 @@
-source scripts/prepare-ci-environment.sh
-./scripts/deploy/deploy.py
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  source scripts/prepare-ci-environment.sh
+  ./scripts/deploy/deploy.py
+fi
