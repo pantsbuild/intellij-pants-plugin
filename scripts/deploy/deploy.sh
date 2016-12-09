0abd031efd38620ad3c51cdc583c3b4b6f01ce5a
wisechengyi <wisechengyi@gmail.com>
2016-12-09 14:02:47 -0800

    restore

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# if [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ]; then
   source scripts/prepare-ci-environment.sh
   ./scripts/deploy/deploy.py
-# fi
+fi
