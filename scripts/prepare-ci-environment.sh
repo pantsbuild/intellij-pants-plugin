76d5a0a6d262742bf60c83197bbf3a9d9450e577
wisechengyi <wisechengyi@gmail.com>
2017-01-25 23:15:38 -0800

    update

--- scripts/prepare-ci-environment.sh
+++ scripts/prepare-ci-environment.sh
@@ -10,8 +10,8 @@ fi
 # Python plugin for Community and Ultimate Edition
 
 export CWD=$(pwd)
-export IJ_VERSION="2016.3.1"
-export IJ_BUILD_NUMBER="163.9166.29"
+export IJ_VERSION="2016.3.3"
+export IJ_BUILD_NUMBER="163.11103.6"
 
 get_md5(){
   if [[ $OSTYPE == *"darwin"* ]]; then
