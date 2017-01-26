24377dc934aefca37f3d18dde0973fb538ed3968
wisechengyi <wisechengyi@gmail.com>
2017-01-26 10:48:28 -0800

    Revert "test some water"
    This reverts commit 70cb415020be27c7285107c36cc92cc94c43a0f6.

--- src/com/twitter/intellij/pants/ui/PantsConsoleManager.java
+++ src/com/twitter/intellij/pants/ui/PantsConsoleManager.java
@@ -67,8 +67,8 @@ public class PantsConsoleManager {
    */
   @TestOnly
   public static void disposeAll() {
-    //for (Map.Entry<Project, ConsoleView> entrySet : mapper.entrySet()) {
-    //  entrySet.getValue().dispose();
-    //}
+    for (Map.Entry<Project, ConsoleView> entrySet : mapper.entrySet()) {
+      entrySet.getValue().dispose();
+    }
   }
 }
