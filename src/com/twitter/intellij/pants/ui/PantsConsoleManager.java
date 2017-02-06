5249b0eb4ff1dd78142cc3888aa662a2260cba71
wisechengyi <wisechengyi@gmail.com>
2017-02-06 00:13:47 -0800

    v2

--- src/com/twitter/intellij/pants/ui/PantsConsoleManager.java
+++ src/com/twitter/intellij/pants/ui/PantsConsoleManager.java
@@ -7,14 +7,8 @@ import com.intellij.execution.filters.TextConsoleBuilderFactory;
 import com.intellij.execution.ui.ConsoleView;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowAnchor;
-import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.ui.content.impl.TabbedContentImpl;
-import com.twitter.intellij.pants.util.PantsConstants;
 import org.jetbrains.annotations.TestOnly;
 
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 
@@ -22,17 +16,7 @@ public class PantsConsoleManager {
   private static ConcurrentHashMap<Project, ConsoleView> mapper = new ConcurrentHashMap<>();
 
   public static void registerConsole(Project project) {
-    //ToolWindow window =
-    //  ToolWindowManager.getInstance(project).registerToolWindow(
-    //    PantsConstants.PANTS_CONSOLE_NAME,
-    //    true,
-    //    ToolWindowAnchor.BOTTOM,
-    //    project,
-    //    true
-    //  );
-    //ConsoleView console = getOrMakeNewConsole(project);
-    //TabbedContentImpl content = new TabbedContentImpl(console.getComponent(), "", true, "");
-    //window.getContentManager().addContent(content);
+    getOrMakeNewConsole(project);
   }
 
   /**
