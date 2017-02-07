4c0e47306f17bb2f721a1a9425dda59f1f5dd9d0
wisechengyi <wisechengyi@gmail.com>
2017-02-07 13:46:56 -0800

    temp

--- tests/com/twitter/intellij/pants/util/PantsUtilTest.java
+++ tests/com/twitter/intellij/pants/util/PantsUtilTest.java
@@ -3,8 +3,12 @@
 
 package com.twitter.intellij.pants.util;
 
+import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.twitter.intellij.pants.testFramework.OSSPantsIntegrationTest;
+
+import java.io.File;
+import java.util.Optional;
 
 public class PantsUtilTest extends OSSPantsIntegrationTest {
 
@@ -13,5 +17,14 @@ public class PantsUtilTest extends OSSPantsIntegrationTest {
     assertTrue(PantsUtil.isPantsProjectFile(LocalFileSystem.getInstance().findFileByPath(getProjectPath())));
     // File system root should not.
     assertFalse(PantsUtil.isPantsProjectFile(LocalFileSystem.getInstance().findFileByPath("/")));
+  }
+
+  public void testFindJdk() {
+    Optional<File> executable = PantsUtil.findPantsExecutable(getProjectFolder());
+    assertTrue(executable.isPresent());
+    Optional<Sdk> sdk_a = PantsUtil.getDefaultJavaSdk(executable.get().getPath());
+    Optional<Sdk> sdk_b = PantsUtil.getDefaultJavaSdk(executable.get().getPath());
+    // Make sure they are identical
+    assertTrue(sdk_a.get() == sdk_b.get());
   }
 }
