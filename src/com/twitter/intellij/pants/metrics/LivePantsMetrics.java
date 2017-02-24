97ab3ba4a9e0b71b8315bf67234866ae0928730b
wisechengyi <wisechengyi@gmail.com>
2017-02-24 14:38:15 -0800

    doc

--- src/com/twitter/intellij/pants/metrics/LivePantsMetrics.java
+++ src/com/twitter/intellij/pants/metrics/LivePantsMetrics.java
@@ -13,29 +13,25 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * This class keeps track of the metrics globally.
- * TODO: keep the metrics per project.
+ * This class captures the indexing duration on the fly, which is independent from the pipeline where
+ * pants export, project loading, and indexing happen in sequence.
+ * <p>
+ * My current understanding about how indexing works, aka project being dumb, is that multiple
+ * components such as python and scala plugins can submit tasks to the framework, from which time to they
+ * are completed, the project is in dumb mode. Also, indexing time can happen in any time and order.
+ * <p>
+ * For example:
+ * <p>
+ * When a project opens, both python and scala plugins found that some .py and .scala files have been modified,
+ * they may submit jobs to update the indexing caches simultaneously. Assuming each job will take 5 seconds and there is
+ * only a single CPU, `PantsDumbModeListener` will report 10 seconds.
+ * <p>
+ * In some other circumstances where files in IDE are not in sync with their counterparts on the disk, there
+ * can be a delay where python plugin detects changes first, and one moment later, scala plugin does the same.
+ * In this case, it is possible `PantsDumbModeListener` will report 5 seconds twice and separately.
  */
 public class LivePantsMetrics {
 
-  /**
-   * This class captures the indexing duration on the fly, which is independent from the pipeline where
-   * pants export, project loading, and indexing happen in sequence.
-   * <p>
-   * My current understanding about how indexing works, aka project being dumb, is that multiple
-   * components such as python and scala plugins can submit tasks to the framework, from which time to they
-   * are completed, the project is in dumb mode. Also, indexing time can happen in any time and order.
-   * <p>
-   * For example:
-   * <p>
-   * When a project opens, both python and scala plugins found that some .py and .scala files have been modified,
-   * they may submit jobs to update the indexing caches simultaneously. Assuming each job will take 5 seconds and there is
-   * only a single CPU, `PantsDumbModeListener` will report 10 seconds.
-   * <p>
-   * In some other circumstances where files in IDE are not in sync with their counterparts on the disk, there
-   * can be a delay where python plugin detects changes first, and one moment later, scala plugin does the same.
-   * In this case, it is possible `PantsDumbModeListener` will report 5 seconds twice and separately.
-   */
   static class PantsDumbModeListener implements DumbService.DumbModeListener {
     private AtomicInteger count = new AtomicInteger(0);
     Stopwatch indexWatch = Stopwatch.createUnstarted();
