6a508a539fccb101cbf2527910a6952be8617aa8
wisechengyi <wisechengyi@gmail.com>
2017-03-22 14:38:43 -0700

    formatting

--- testData/quickfix/addPantsTargetDependency/basic_expected.py
+++ testData/quickfix/addPantsTargetDependency/basic_expected.py
@@ -2,8 +2,8 @@ java_library(name='test',
   dependencies = [
     'bar/baz1',
     'bar/baz2',
-    'bar/baz3:baz3',
+      'bar/baz3:baz3',
     'foo:scala',
-  ],
+],
   sources=rglobs('*.java'),
 )
