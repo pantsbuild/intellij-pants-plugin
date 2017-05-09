6bb318d4fba5384bce74a3b502a169ffdaa9e1e4
wisechengyi <wisechengyi@gmail.com>
2017-05-08 22:17:55 -0700

    or tag

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]; then
   source scripts/prepare-ci-environment.sh
   ./scripts/deploy/deploy.py --tag="$TRAVIS_TAG"
 else
