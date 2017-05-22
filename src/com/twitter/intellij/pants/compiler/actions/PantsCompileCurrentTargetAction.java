865b7f4177bf0931ce55b603ab726abb82b7e27c
wisechengyi <wisechengyi@gmail.com>
2017-05-21 19:47:15 -0700

    add compile current target action

---
+++ src/com/twitter/intellij/pants/compiler/actions/PantsCompileCurrentTargetAction.java
@@ -0,0 +1,50 @@
+// Copyright 2017 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+package com.twitter.intellij.pants.compiler.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.twitter.intellij.pants.util.PantsUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * PantsCompileAllTargetsAction is a UI action that, when in a project, compiles all targets in the project
+ */
+public class PantsCompileCurrentTargetAction extends PantsCompileActionBase {
+
+  protected PantsCompileCurrentTargetAction(String name) {
+    super(name);
+  }
+
+  public PantsCompileCurrentTargetAction() {
+    this("Compile target(s) in the selected editor");
+  }
+
+  /**
+   * Find the target(s) that are only associated with the file opened in the selected editor.
+   */
+  @NotNull
+  @Override
+  public Stream<String> getTargets(@NotNull AnActionEvent e, @NotNull Project project) {
+    VirtualFile[] files = FileEditorManager.getInstance(project).getSelectedFiles();
+    Set<Module> relatedModules =
+      Arrays.stream(files)
+        .map(x -> ProjectRootManager.getInstance(project).getFileIndex().getModuleForFile(x))
+        .collect(Collectors.toSet());
+
+    return relatedModules.stream()
+      .map(PantsUtil::getNonGenTargetAddresses)
+      .flatMap(Collection::stream);
+  }
+}
