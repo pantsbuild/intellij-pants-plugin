ce8ab0624526ada6206ec2a9e50a467f532a2e50
wisechengyi <wisechengyi@gmail.com>
2017-07-19 23:47:52 -0700

    remove get 3rdparty script

--- scripts/get_3rdparty_jars.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-output = subprocess.check_output("./pants dependencies : 3rdparty/intellij:: | sort | grep 'com\|org\|jdk'", shell=True)
-
-for l in output.splitlines():
-  org, package, _ = l.split(':')
-  # print org, package
-  print "exclude('{}', '{}'),".format(org, package)
