cde24d68a635b910f725e4a197c8d13049af15b6
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-02-22 12:59:12 -0800

    update to 2017.3.4

--- scripts/prepare-ci-environment.sh
+++ scripts/prepare-ci-environment.sh
@@ -10,8 +10,8 @@ fi
 # Python plugin for Community and Ultimate Edition
 
 export CWD=$(pwd)
-export IJ_VERSION="2017.3.1"
-export IJ_BUILD_NUMBER="173.3942.27"
+export IJ_VERSION="2017.3.4"
+export IJ_BUILD_NUMBER="173.4548.28"
 
 get_md5(){
   if [[ $OSTYPE == *"darwin"* ]]; then
@@ -24,20 +24,20 @@ get_md5(){
 if [[ "${IJ_ULTIMATE:-false}" == "true" ]]; then
   export IJ_BUILD="IU-${IJ_VERSION}"
   export FULL_IJ_BUILD_NUMBER="IU-${IJ_BUILD_NUMBER}"
-  export EXPECTED_IJ_MD5="9c465600198d76642d45e2c9fefc27d7"
+  export EXPECTED_IJ_MD5="96beb7f3b0e1f884104aa70cf72a4a2f"
   export PYTHON_PLUGIN_ID="Pythonid"
-  export PYTHON_PLUGIN_MD5="cfdce2031ab782bfa8891248de807dd3"
+  export PYTHON_PLUGIN_MD5="c90c79d802cfae67aea616c2127c9657"
 else
   export IJ_BUILD="IC-${IJ_VERSION}"
   export FULL_IJ_BUILD_NUMBER="IC-${IJ_BUILD_NUMBER}"
-  export EXPECTED_IJ_MD5="1d322650188899200b9e70b3f7d8fd56"
+  export EXPECTED_IJ_MD5="84e7374f7cab5b5e5a9c202fc4f046ca"
   export PYTHON_PLUGIN_ID="PythonCore"
-  export PYTHON_PLUGIN_MD5="f82f18c33c35c3320374d9bb25664aa8"
+  export PYTHON_PLUGIN_MD5="85a5b2addeb195f82379a928d3b3a2fe"
 fi
 
 # we will use Community ids to download plugins.
 export SCALA_PLUGIN_ID="org.intellij.scala"
-export SCALA_PLUGIN_MD5="c4672fb856eda55657fcdd99cbf82e3b"
+export SCALA_PLUGIN_MD5="e3446c71e42f1844b1dbc45c207b007b"
 
 export INTELLIJ_PLUGINS_HOME="$CWD/.cache/intellij/$FULL_IJ_BUILD_NUMBER/plugins"
 export INTELLIJ_HOME="$CWD/.cache/intellij/$FULL_IJ_BUILD_NUMBER/idea-dist"
