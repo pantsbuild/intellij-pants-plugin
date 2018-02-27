b2628616e24b08f2e6ff22ce15ceaab15b9b0fd0
wisechengyi <wisechengyi@gmail.com>
2018-02-27 13:11:58 -0800

    update test data

--- testData/testprojects/intellij-integration/tests/java/org/pantsbuild/cp_print/AppTest.java
+++ testData/testprojects/intellij-integration/tests/java/org/pantsbuild/cp_print/AppTest.java
@@ -1,11 +1,13 @@
 package org.pantsbuild.testproject.cp_print;
 
+import com.google.common.collect.Sets;
 import org.junit.Test;
 
 import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -20,11 +22,16 @@ public class AppTest {
 
     URL[] urls = ((URLClassLoader) cl).getURLs();
 
-    Set<String> jarNames = Arrays.stream(urls).map(u -> new File(u.getPath()).getName()).collect(Collectors.toSet());
+    Set<String> jarNames = Arrays.stream(urls)
+      .map(u -> new File(u.getPath()).getName()).collect(Collectors.toSet());
 
-    assertTrue(jarNames.contains("manifest.jar"));
+    assertTrue(jarNames.toString(), jarNames.contains("manifest.jar"));
 
     // autovalue imported into the project should not be directly be on the classpath
-    assertFalse(jarNames.stream().anyMatch(s -> s.contains("auto-value")));
+    assertFalse(jarNames.toString(), jarNames.contains("guava-23.0.jar"));
+
+    // Make sure guava is accessible, so it is not coming from "guava-23.0.jar"
+    // directly on the classpath, but from Pants' manifest.jar
+    HashSet<Object> objects = Sets.newHashSet();
   }
 }
