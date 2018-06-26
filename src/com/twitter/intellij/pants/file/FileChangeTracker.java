36041fd5c7779d31583a54c91d8c430c82dcd840
Dorothy Ordogh <dordogh@twitter.com>
2018-06-25 17:45:31 -0700

    ensure that a gradle build file isn't in a pants project so that we don't suggest refreshing the gradle project like we do pants

--- src/com/twitter/intellij/pants/file/FileChangeTracker.java
+++ src/com/twitter/intellij/pants/file/FileChangeTracker.java
@@ -145,7 +145,7 @@ public class FileChangeTracker {
       }
     };
 
-    if (PantsUtil.isBUILDFileName(file.getName())) {
+    if (PantsUtil.isFileUnderPantsProject(file) && PantsUtil.isBUILDFileName(file.getName())) {
       Notification myNotification = new Notification(
         PantsConstants.PANTS,
         PantsBundle.message("pants.project.build.files.changed"),
