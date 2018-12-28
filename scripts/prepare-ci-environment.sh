92023e8f975afda3c80e9714675d614ee5c160e8
wisechengyi <wisechengyi@gmail.com>
2018-12-28 11:42:39 -0800

    sha

--- scripts/prepare-ci-environment.sh
+++ scripts/prepare-ci-environment.sh
@@ -30,13 +30,13 @@ get_md5(){
 if [[ "${IJ_ULTIMATE:-false}" == "true" ]]; then
   export IJ_BUILD="IU-${IJ_VERSION}"
   export FULL_IJ_BUILD_NUMBER="IU-${IJ_BUILD_NUMBER}"
-  export EXPECTED_IJ_MD5="e86dfce06b011f391bf6c54fb25c22da"
+  export EXPECTED_IJ_MD5="06dac318478f95335c142bebab586de5"
   export PYTHON_PLUGIN_ID="Pythonid"
   export PYTHON_PLUGIN_MD5="eb942096057c2c5dece681254d8abaf1"
 else
   export IJ_BUILD="IC-${IJ_VERSION}"
   export FULL_IJ_BUILD_NUMBER="IC-${IJ_BUILD_NUMBER}"
-  export EXPECTED_IJ_MD5="06dac318478f95335c142bebab586de5"
+  export EXPECTED_IJ_MD5="7bf14969ca97b574ecb8eb3844d9816e"
   export PYTHON_PLUGIN_ID="PythonCore"
   export PYTHON_PLUGIN_MD5="6fbabbbc179b022c1b5fd6e37b645b45"
 fi
