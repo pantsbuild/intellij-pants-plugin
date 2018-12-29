3d174207f509a90e5bc8fd9ce93ac66298c3e28d
Yi Cheng <wisechengyi@gmail.com>
2018-12-28 20:54:21 -0800

    remove index cache

--- scripts/run-tests-ci.sh
+++ scripts/run-tests-ci.sh
@@ -6,6 +6,9 @@ source scripts/prepare-ci-environment.sh
 rm -rf .cache/dummy_repo
 cp -r testData/dummy_repo .cache/
 
+# Remove IntelliJ index cache.
+rm -rf .cache/intellij/*/idea-dist/system/caches/
+
 args="${TASKS:-test} tests:${TEST_SET:-:} $(append_intellij_jvm_options test-junit) ${ADDITIONAL_ARGS:-$@}"
 
 echo "Running ./pants $args"
