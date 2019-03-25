34e0be008498a23a1fd340c09e159fec070137b4
wisechengyi <wisechengyi@gmail.com>
2019-03-25 15:44:31 -0700

    fix setup

--- scripts/setup-ci-environment.sh
+++ scripts/setup-ci-environment.sh
@@ -36,7 +36,6 @@ if [ ! -d .cache/intellij/$FULL_IJ_BUILD_NUMBER/idea-dist ]; then
   IJ_TAR_NAME=idea${IJ_BUILD}.tar.gz
   echo "Loading $IJ_BUILD..."
   wget -q -O $IJ_TAR_NAME "https://download.jetbrains.com/idea/$IJ_TAR_NAME"
-  verify_md5 $IJ_TAR_NAME $EXPECTED_IJ_MD5
   {
     tar zxf $IJ_TAR_NAME &&
     UNPACKED_IDEA=$(find . -name 'idea-I*' | head -n 1) &&
@@ -55,12 +54,10 @@ if [ ! -d .cache/intellij/$FULL_IJ_BUILD_NUMBER/plugins ]; then
   pushd plugins
 
   wget -q --no-check-certificate -O Scala.zip "https://plugins.jetbrains.com/pluginManager/?action=download&id=$SCALA_PLUGIN_ID&build=$FULL_IJ_BUILD_NUMBER"
-  verify_md5 Scala.zip $SCALA_PLUGIN_MD5
   unzip -q Scala.zip
   rm -f Scala.zip
 
   wget -q --no-check-certificate  -O python.zip "https://plugins.jetbrains.com/pluginManager/?action=download&id=$PYTHON_PLUGIN_ID&build=$FULL_IJ_BUILD_NUMBER"
-  verify_md5 python.zip $PYTHON_PLUGIN_MD5
   unzip -q python.zip
   rm -f python.zip
 
