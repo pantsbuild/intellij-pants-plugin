e74ce17b082979b9d0982956ed5ef58b9f5a4985
Marek Żarnowski <mzarnowski@virtuslab.com>
2019-11-13 11:11:43 +0100

    Replace Sdk with a Jdk reference as the project data

--- src/com/twitter/intellij/pants/service/project/wizard/PantsProjectImportBuilder.java
+++ src/com/twitter/intellij/pants/service/project/wizard/PantsProjectImportBuilder.java
@@ -4,17 +4,15 @@
 package com.twitter.intellij.pants.service.project.wizard;
 
 import com.intellij.ide.util.projectWizard.WizardContext;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.externalSystem.model.DataNode;
 import com.intellij.openapi.externalSystem.model.project.ProjectData;
 import com.intellij.openapi.externalSystem.service.project.ProjectDataManager;
 import com.intellij.openapi.externalSystem.service.project.wizard.AbstractExternalProjectImportBuilder;
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.projectRoots.JavaSdk;
-import com.intellij.openapi.projectRoots.ProjectJdkTable;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.twitter.intellij.pants.PantsBundle;
+import com.twitter.intellij.pants.model.JdkRef;
 import com.twitter.intellij.pants.settings.ImportFromPantsControl;
 import com.twitter.intellij.pants.util.PantsConstants;
 import icons.PantsIcons;
@@ -22,7 +20,6 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.Icon;
 import java.io.File;
-import java.util.List;
 
 public class PantsProjectImportBuilder extends AbstractExternalProjectImportBuilder<ImportFromPantsControl> {
 
@@ -65,31 +62,10 @@ public class PantsProjectImportBuilder extends AbstractExternalProjectImportBuil
       return;
     }
 
-    final DataNode<Sdk> sdkNode = ExternalSystemApiUtil.find(node, PantsConstants.SDK_KEY);
+    final DataNode<JdkRef> sdkNode = ExternalSystemApiUtil.find(node, PantsConstants.SDK_KEY);
     if (sdkNode != null) {
-      Sdk pantsSdk = sdkNode.getData();
-      context.setProjectJdk(addIfNotExists(pantsSdk));
+      Sdk pantsSdk = sdkNode.getData().toSdk();
+      context.setProjectJdk(pantsSdk);
     }
-  }
-
-  /**
-   * Find if pants sdk is already configured, return the existing sdk if it exists,
-   * otherwise add to the config and return.
-   */
-  private Sdk addIfNotExists(final Sdk pantsSdk) {
-    final JavaSdk javaSdk = JavaSdk.getInstance();
-    List<Sdk> sdks = ProjectJdkTable.getInstance().getSdksOfType(javaSdk);
-    for (Sdk sdk : sdks) {
-      if (javaSdk.getVersion(sdk) == javaSdk.getVersion(pantsSdk)) {
-        return sdk;
-      }
-    }
-    ApplicationManager.getApplication().runWriteAction(new Runnable() {
-      @Override
-      public void run() {
-        ProjectJdkTable.getInstance().addJdk(pantsSdk);
-      }
-    });
-    return pantsSdk;
   }
 }
