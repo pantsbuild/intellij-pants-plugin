c1d05ca72fe69735f320a2781ac2af03d25c2f6c
Marek Żarnowski <marek1840@gmail.com>
2019-11-18 08:28:43 +0100

    Remove jdks after the test case

--- tests/com/twitter/intellij/pants/model/JdkRefTest.java
+++ tests/com/twitter/intellij/pants/model/JdkRefTest.java
@@ -8,15 +8,17 @@ import com.intellij.openapi.projectRoots.JavaSdk;
 import com.intellij.openapi.projectRoots.ProjectJdkTable;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.impl.MockSdk;
-import com.intellij.testFramework.LightPlatformTestCase;
 import com.intellij.util.containers.MultiMap;
+import com.twitter.intellij.pants.testFramework.PantsIntegrationTestCase;
 import com.twitter.intellij.pants.util.PantsUtil;
 
 import java.util.Set;
 
-public class JdkRefTest extends LightPlatformTestCase {
+public class JdkRefTest extends PantsIntegrationTestCase {
+  private static final String SDK_NAME = "mySDK";
+
   public void testResolvesToExistingSdk() {
-    Sdk originalSdk = PantsUtil.createJdk("myName", "myHome", getTestRootDisposable());
+    Sdk originalSdk = PantsUtil.createJdk(SDK_NAME, "myHome", getTestRootDisposable());
     JdkRef ref = JdkRef.fromSdk(originalSdk);
 
     Sdk resolvedSdk = ref.toSdk(getTestRootDisposable());
@@ -25,12 +27,18 @@ public class JdkRefTest extends LightPlatformTestCase {
   }
 
   public void testRegistersSdkIfNotPresent() {
-    Sdk sdk = new MockSdk("myName", "myHome", "myVersion", MultiMap.empty(), JavaSdk.getInstance());
+    Sdk sdk = new MockSdk(SDK_NAME, "myHome", "myVersion", MultiMap.empty(), JavaSdk.getInstance());
     JdkRef ref = JdkRef.fromSdk(sdk);
 
     Sdk resolvedSdk = ref.toSdk(getTestRootDisposable());
 
     Set<Sdk> registeredSdks = Sets.newHashSet(ProjectJdkTable.getInstance().getAllJdks());
     assertTrue(registeredSdks.contains(resolvedSdk));
+  }
+
+  @Override
+  public void tearDown() throws Exception {
+    removeJdks(jdk -> jdk.getName().equals(SDK_NAME));
+    super.tearDown();
   }
 }
