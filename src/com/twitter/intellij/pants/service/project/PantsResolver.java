cc097b1a54a3cb054d736e0a3791616d820159c3
wisechengyi <wisechengyi@gmail.com>
2019-12-20 21:05:51 -0800

    bump resolve version

--- src/com/twitter/intellij/pants/service/project/PantsResolver.java
+++ src/com/twitter/intellij/pants/service/project/PantsResolver.java
@@ -37,7 +37,7 @@ public class PantsResolver {
    * Bump this version if project resolve changes. It will prompt user to refresh.
    * E.g. more modules are created or their relationship changes.
    */
-  public static final int VERSION = 15;
+  public static final int VERSION = 16;
 
   protected static final Logger LOG = Logger.getInstance(PantsResolver.class);
   protected final PantsCompileOptionsExecutor myExecutor;
