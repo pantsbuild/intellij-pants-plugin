d80e18dab36dff50cb8636fd0daab3aa7ff9ff51
wisechengyi <wisechengyi@gmail.com>
2020-01-09 16:31:51 -0800

    imports

--- tests/com/twitter/intellij/pants/integration/OSSPantsIdeaPluginGoalIntegrationTest.java
+++ tests/com/twitter/intellij/pants/integration/OSSPantsIdeaPluginGoalIntegrationTest.java
@@ -4,7 +4,6 @@
 package com.twitter.intellij.pants.integration;
 
 import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.junit.JUnitConfiguration;
 import com.intellij.execution.process.ProcessOutput;
 import com.intellij.ide.highlighter.ProjectFileType;
 import com.intellij.ide.impl.ProjectUtil;
@@ -17,14 +16,12 @@ import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.util.ui.UIUtil;
 import com.twitter.intellij.pants.testFramework.OSSPantsIntegrationTest;
 import com.twitter.intellij.pants.util.PantsUtil;
-import org.junit.Ignore;
 
 import java.io.File;
 import java.util.Arrays;
 import java.util.Optional;
 
 
-//@Ignore("https://github.com/pantsbuild/intellij-pants-plugin/issues/402")
 public class OSSPantsIdeaPluginGoalIntegrationTest extends OSSPantsIntegrationTest {
 
   public void testPantsIdeaPluginGoal() throws Throwable {
