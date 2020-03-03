05ce421100e25cf0352d977a95b24a2b6c5dda64
Marek Żarnowski <mzarnowski@virtuslab.com>
2020-03-03 11:45:35 +0100

    Bump intellij version to latest eap: 201.5985.32

--- scripts/prepare-ci-environment.sh
+++ scripts/prepare-ci-environment.sh
@@ -12,8 +12,8 @@ fi
 export CWD=$(pwd)
 # Normally, IJ_VERSION is of the form YEAR.x[.y[.z]]
 # But for EAPs, set IJ_VERSION to the same as IJ_BUILD_NUMBER
-export IJ_VERSION="2019.3.3"
-export IJ_BUILD_NUMBER="193.6494.35"
+export IJ_VERSION="201.5259.13"
+export IJ_BUILD_NUMBER="201.5259.13"
 
 # This is for bootstrapping Pants, since this repo does not do Pants intensive operations,
 # we can optimize for build time.
