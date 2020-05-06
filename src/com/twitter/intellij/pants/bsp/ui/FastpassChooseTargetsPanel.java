e9cc9501a6e1871004ea52d2d878820e3a643de9
Tomasz Pasternak <tpasternak@virtuslab.com>
2020-05-06 14:54:39 +0200

    New whitespace

--- src/com/twitter/intellij/pants/bsp/ui/FastpassChooseTargetsPanel.java
+++ src/com/twitter/intellij/pants/bsp/ui/FastpassChooseTargetsPanel.java
@@ -196,9 +196,11 @@ class FastpassChooseTargetsPanel extends JPanel {
     switch (targetAddress.getKind()){
       case SINGLE_TARGET: return CompletableFuture.completedFuture(Collections.singletonList(targetAddress));
       case ALL_TARGETS_DEEP: return myTargetsListFetcher.apply(resolvePathToFile(targetAddress.getPath()));
-      case ALL_TARGETS_FLAT: return myTargetsListFetcher.apply(resolvePathToFile(targetAddress.getPath())).thenApply(x -> x.stream().filter(t -> t.getPath() == targetAddress.getPath()).collect(
-        Collectors.toSet()));  // todo handle flat
+      case ALL_TARGETS_FLAT: return myTargetsListFetcher.apply(resolvePathToFile(targetAddress.getPath()))
+        .thenApply(x -> x.stream()
+          .filter(t -> t.getPath() == targetAddress.getPath())
+          .collect(Collectors.toSet()));  // todo handle flat
     }
-    return null;
+    return null; // todo no null
   }
 }
