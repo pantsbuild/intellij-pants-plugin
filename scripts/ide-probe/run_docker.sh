6a3226e68a810ddd8007faa6e5ad118633c52974
Łukasz Wawrzyk <lwawrzyk@virtuslab.com>
2020-06-24 17:11:40 +0200

    Initial setup of ideprobe

---
+++ scripts/ide-probe/run_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+find -maxdepth 1 -regex '.*/pants_.*\.zip' -delete
+TRAVIS_BRANCH=master ./scripts/deploy/deploy.sh --skip-publish
+PANTS_ZIP=$(ls | grep 'pants_.*\.zip' | head -1 | head -c -1)
+
+DOCKER_IMAGE=ide-probe
+DOCKER_DIRECTORY=/tmp/ideprobe/output
+HOST_DIRECTORY=/tmp/ideprobe/output
+
+mkdir -p "${HOST_DIRECTORY}"
+docker run  \
+  --mount type=bind,source="${HOST_DIRECTORY}",target="${DOCKER_DIRECTORY}" \
+  -v ${PWD}/$PANTS_ZIP:/tmp/pants.zip \
+  "${DOCKER_IMAGE}" \
+  "$@"
