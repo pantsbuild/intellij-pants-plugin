5d35fe9ab7a19a7de5e36acbb81c7bca803722ba
Yi Cheng <wisechengyi@gmail.com>
2020-07-21 10:01:04 -0700

    Allow deploy on a branch (#550)

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Trigger deploy process if travis ci build is on master or on a tag.
-if [ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]; then
+if [ "$TRAVIS_BRANCH" == "release/1.15.1_patches" ] || [ ! -z "$TRAVIS_TAG" ]; then
   source scripts/prepare-ci-environment.sh
   python3 ./scripts/deploy/deploy.py --tag="$TRAVIS_TAG" "$@"
 else
