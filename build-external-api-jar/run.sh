f0a8697ba764a1a3177485b029e1c8b0b97efa3d
Tomasz Pasternak <tpasternak@virtuslab.com>
2021-01-21 14:32:52 +0100

    Path to license in git root

--- build-external-api-jar/run.sh
+++ build-external-api-jar/run.sh
@@ -6,6 +6,7 @@ IDEA_VERSION=203.5981.114
 IDEA_SHA=94b17f42465b8f2af7bc8b9bf0ced1b9989877596d1c2758e4104b089b931b55
 SOURCES_DIR="/tmp/idea-sources"
 DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+GIT_ROOT=$DIR/..
 
 set -e
 (
@@ -24,6 +25,6 @@ set -e
     cd /tmp
     cp $SOURCES_DIR/LICENSE.txt intellij_license.txt
     cp $SOURCES_DIR/NOTICE.txt intellij_notice.txt
-    cp "$DIR/LICENSE" license_from_build_project.txt
+    cp "$GIT_ROOT/LICENSE" license_from_build_project.txt
     zip -xi external-system-test-api.zip -xi external-system-test-api.jar intellij_notice.txt intellij_license.txt license_from_build_project.txt
 )
