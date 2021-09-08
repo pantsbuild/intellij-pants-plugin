ad0059d144a0073bf29df0ce223bc760805d1dce
Tomasz Pasternak <tpasternak@virtuslab.com>
2021-09-08 10:29:52 +0200

    Test tokens

--- scripts/deploy/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Trigger deploy process if travis ci build is on master or on a tag.
-if [ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]; then
-  source scripts/prepare-ci-environment.sh
-  python3 ./scripts/deploy/deploy.py --tag="$TRAVIS_TAG" "$@"
-else
-  echo "Not on master. Skip deployment."
-fi
