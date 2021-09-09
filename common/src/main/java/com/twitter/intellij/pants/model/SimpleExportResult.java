4a593cc0c8aeec03f75dcd431d827408d6e281be
Kamil Doległo <kamilok1965@interia.pl>
2021-09-09 16:45:47 +0200

    Remove unnecessary logging

--- common/src/main/java/com/twitter/intellij/pants/model/SimpleExportResult.java
+++ common/src/main/java/com/twitter/intellij/pants/model/SimpleExportResult.java
@@ -88,7 +88,7 @@ public class SimpleExportResult {
         String.format("%s=%s", PantsConstants.PANTS_CLI_OPTION_EXPORT_OUTPUT_FILE,
                       tempFile.getFile().getPath()));
       final ProcessOutput processOutput = PantsUtil.getCmdOutput(commandline, null);
-      System.out.println(processOutput);
+
       if (processOutput.checkSuccess(LOG)) {
         SimpleExportResult result = parse(FileUtil.loadFile(tempFile.getFile()));
         simpleExportCache.put(pantsExecutableFile, result);
