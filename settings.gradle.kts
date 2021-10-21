c1ac25963253ffd9c8056e81a3e0274c12958e19
Łukasz Wawrzyk <lwawrzyk@virtuslab.com>
2021-10-21 16:54:48 +0200

    Rename plugin directory to pants (#648)
    Co-authored-by: Lukasz Wawrzyk <uwawrzyk@twitter.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,5 +7,5 @@
  * in the user manual at https://docs.gradle.org/7.1.1/userguide/multi_project_builds.html
  */
 
-rootProject.name = "intellij-pants-plugin"
+rootProject.name = "pants"
 include("common", "testFramework", "externalTestApi")
